f616656d7753d0ff8d4d3eca30c41d8cd4f0ed98
zankoav <zankoav@mail.ru>
2019-07-08 00:33:32 +0300

    update

--- mastak/init.php
+++ mastak/init.php
@@ -1129,10 +1129,10 @@
 
     function house_archive_per_page( $query ) {
         if ( is_archive( 'house' ) ) {
-            $query->set( 'meta_key', 'mastak_house_order' );
-            $query->set( 'orderby', 'meta_value_num' );
-            $query->set( 'order', 'DESC' );
+//            $query->set( 'meta_key', 'mastak_house_order' );
+//            $query->set( 'orderby', 'meta_value_num' );
+//            $query->set( 'order', 'DESC' );
         }
-        return $query;
+//        return $query;
     }
     add_filter( 'pre_get_posts', 'house_archive_per_page' );
