c366e4d67259e75088174aeb5c77c328d2d83e34
zankoav <zankoav@mail.ru>
2019-08-05 23:46:58 +0300

    update

--- footer-mastak.php
+++ footer-mastak.php
@@ -189,9 +189,14 @@
             var email = getCookie('kg_email');
             var phone = getCookie('kg_phone');
 
+            console.log(email,name,phone);
+
+
             var $inputName = $('[name="your-name"]');
             var $inputPhone = $('[name="tel"]');
             var $inputEmail = $('[name="your-email"]');
+
+            console.log($inputName.val(),$inputPhone.val(),$inputEmail.val());
 
             if(name && $inputName.val() != ''){
                 $inputName.val(name);
@@ -214,30 +219,31 @@
             return matches ? decodeURIComponent(matches[1]) : undefined;
         }
 
-        function setCookie(name, value, options = {}) {
-
-            options = {
-                path: '/',
-                // при необходимости добавьте другие значения по умолчанию
-                ...options
-            };
-
-            if (options.expires.toUTCString) {
-                options.expires = options.expires.toUTCString();
+        function setCookie(name, value, props) {
+            props = props || {}
+            var exp = props.expires
+            if (typeof exp == "number" && exp) {
+                var d = new Date()
+                d.setTime(d.getTime() + exp*1000)
+                exp = props.expires = d
             }
 
-            let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+            if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
+            value = encodeURIComponent(value)
+            var updatedCookie = name + "=" + value
+            for(var propName in props){
 
-            for (let optionKey in options) {
-                updatedCookie += "; " + optionKey;
-                let optionValue = options[optionKey];
-                if (optionValue !== true) {
-                    updatedCookie += "=" + optionValue;
-                }
+                updatedCookie += "; " + propName
+
+                var propValue = props[propName]
+
+                if(propValue !== true){ updatedCookie += "=" + propValue }
             }
 
-            document.cookie = updatedCookie;
+            document.cookie = updatedCookie
+
         }
+
     })(jQuery);
 </script>
 <!-- Yandex.Metrika counter -->
