49c36abcd45f5ed014a06c71f0259f0b6c45c219
zankoav <zankoav@mail.ru>
2019-08-24 08:53:35 +0300

    update

--- inc/calendar/js/public.js
+++ inc/calendar/js/public.js
@@ -139,7 +139,7 @@ function checkDateRange(events, startDate, endDate) {
     for(var i = 0; i < events.length; i++){
         var event = events[i];
         var startEvent = event.start;
-        var endEvent = event.end.subtract(1, 'days');
+        var endEvent = moment(event.end, 'YYYY-MM-DD').subtract(1, 'days').format('YYYY-MM-DD');
 
         if(startDate < endEvent && endDate > startEvent){
             result = false;
