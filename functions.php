ad77e2b06ed9a2d79bfa083619affbe12adc0b4e
zankoav <zankoav@mail.ru>
2019-09-12 23:26:01 +0300

    update

--- functions.php
+++ functions.php
@@ -7,10 +7,3 @@
     }
     require __DIR__ . '/inc/calendar/init.php';
     require __DIR__ . '/mastak/init.php';
-
-    function bybe_remove_yoast_json($data){
-//        $data = array();
-        var_dump($data);
-        return $data;
-    }
-    add_filter('wpseo_json_ld_output', 'bybe_remove_yoast_json', 10, 1);
