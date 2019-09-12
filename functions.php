1ee33789e79c3acb617b69a54b17846cebd31171
zankoav <zankoav@mail.ru>
2019-09-12 23:14:08 +0300

    update

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@
     require __DIR__ . '/mastak/init.php';
 
     function bybe_remove_yoast_json($data){
-        $data = array();
+//        $data = array();
+        var_dump($data);
         return $data;
     }
     add_filter('wpseo_json_ld_output', 'bybe_remove_yoast_json', 10, 1);
