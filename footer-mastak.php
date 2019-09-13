7baa920c6b4882992cc7e24c036817c610c9233e
zankoav <zankoav@mail.ru>
2019-09-13 23:16:24 +0300

    update

--- footer-mastak.php
+++ footer-mastak.php
@@ -16,9 +16,6 @@
                     },
                     body: data
                 })
-                    .then(function (response) {
-                        return response.json()
-                    })
                     .then(function (result) {
                         console.log('result', result);
 
