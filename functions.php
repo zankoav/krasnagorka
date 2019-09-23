3c73422841e12bfc28cc87c7d86f3122279729d5
zankoav <zankoav@mail.ru>
2019-09-23 22:06:06 +0300

    OK

--- functions.php
+++ functions.php
@@ -8,15 +8,16 @@
     require __DIR__ . '/inc/calendar/init.php';
     require __DIR__ . '/mastak/init.php';
     
-//    add_filter( 'wpseo_schema_graph_pieces', function($pieces, $context){
-//
-//        if(get_the_ID() == 10188){
+    add_filter( 'wpseo_schema_graph_pieces', function($pieces, $context){
+
+        if(get_the_ID() == 10188){
+            var_dump($pieces[0]);
 //            unset($pieces[0]);
 //            unset($pieces[2]);
 //            unset($pieces[3]);
 //            unset($pieces[4]);
 //            unset($pieces[5]);
-//        }
-//
-//		return $pieces;
-//    } , 20, 2 );
+        }
+
+		return $pieces;
+    } , 20, 2 );
