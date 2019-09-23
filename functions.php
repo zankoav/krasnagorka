cc50432683ba88bc22d2f93551846ea8d1fce82f
zankoav <zankoav@mail.ru>
2019-09-23 22:10:14 +0300

    OK

--- functions.php
+++ functions.php
@@ -7,17 +7,22 @@
     }
     require __DIR__ . '/inc/calendar/init.php';
     require __DIR__ . '/mastak/init.php';
-    
-    add_filter( 'wpseo_schema_graph_pieces', function($pieces, $context){
 
-        if(get_the_ID() == 10188){
+    add_filter('wpseo_schema_graph_pieces', function ($pieces, $context) {
+
+        if (get_the_ID() == 10188) {
             var_dump($pieces[0]);
-//            unset($pieces[0]);
-//            unset($pieces[2]);
-//            unset($pieces[3]);
-//            unset($pieces[4]);
-//            unset($pieces[5]);
+            $pieces[0]['aggregateRating'] = [
+                '@type'       => 'AggregateRating',
+                'ratingValue' => 4.5,
+                'ratingCount' => 120
+            ];
+            //            unset($pieces[0]);
+            //            unset($pieces[2]);
+            //            unset($pieces[3]);
+            //            unset($pieces[4]);
+            //            unset($pieces[5]);
         }
 
-		return $pieces;
-    } , 20, 2 );
+        return $pieces;
+    }, 20, 2);
