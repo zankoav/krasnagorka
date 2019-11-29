c4cee6690ac97f08de1a6d2b37f7038d80ea9a6b
zankoav <zankoav@mail.ru>
2019-11-29 14:54:11 +0300

    update

--- page-booking-form.php
+++ page-booking-form.php
@@ -12,7 +12,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Бронирование</title>
+    <link rel="shortcut icon" href="<?= get_site_icon_url();?>" type="image/x-icon">
+    <title><?= get_the_title();?></title>
     <link rel="stylesheet" href="<?= $assets->css('booking'); ?>">
 </head>
 <body>
