ea93b2a03cfcb55d4c63cd62ea479b62831b9636
zankoav <zankoav@mail.ru>
2019-11-29 10:42:55 +0300

    update

--- page-booking-form.php
+++ page-booking-form.php
@@ -3,6 +3,7 @@
     if (!defined('ABSPATH')) {
         exit;
     }
+    $bookingModel = $model->getBookingModel();
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,13 +13,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Бронирование</title>
-    <link rel="stylesheet" href="<?=$assets->css('booking');?>">
+    <link rel="stylesheet" href="<?= $assets->css('booking'); ?>">
 </head>
 <body>
 <script>
-    const model = `<?=$model->getBookingModel();?>`;
+    const model = `<?=$bookingModel;?>`;
 </script>
-<script src="<?=$assets->js('booking');?>"></script>
+<script src="<?= $assets->js('booking'); ?>"></script>
 </body>
 </html>
 
