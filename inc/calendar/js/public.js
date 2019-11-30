7ca891c9f824c569d26333ce83f5c7f63bffbf9b
zankoav <zankoav@mail.ru>
2019-11-30 20:31:46 +0300

    update

--- inc/calendar/js/public.js
+++ inc/calendar/js/public.js
@@ -135,9 +135,7 @@ function loadCalendar() {
                             jQuery($orderButton[0]).attr('href', baseHref);
                         }
 
-                        if($teremButton.length){
-                            console.log(event);
-                            
+                        if($teremButton.length && event.target.classList.indexOf('terem-button') > -1){
                             const bookingId = jQuery($teremButton[0]).data('id');
                             const baseHref = `/booking-form/?booking=${bookingId}`;
                             jQuery($teremButton[0]).attr('href', baseHref);
