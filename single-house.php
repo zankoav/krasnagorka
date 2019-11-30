423bb1cde6f483a9748389bef9cb453845f4672b
zankoav <zankoav@mail.ru>
2019-11-30 21:32:59 +0300

    update

--- single-house.php
+++ single-house.php
@@ -221,7 +221,7 @@
                             <div class="booking-houses__icons-description">
                                 <?php get_template_part('mastak/views/icons-description'); ?>
                                 <div class="select-helper select-helper_header">
-                                    <img src="//mastak/assets/icons/date-clicking-selecting.png" class="select-helper__img" alt="Выделение дат заезда и выезда">
+                                    <img src="/wp-content/themes/krasnagorka/mastak/assets/icons/date-clicking-selecting.png" class="select-helper__img" alt="Выделение дат заезда и выезда">
                                     <p class="select-helper__text"><?=get_option('mastak_theme_options')['calendar_settings_message_before'];?></p>
                                 </div>
                             </div>
