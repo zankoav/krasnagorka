0be547a2a643d032840a7de3537a6da15604aeb6
zankoav <zankoav@mail.ru>
2020-02-13 23:44:11 +0300

    gi

--- lwc/frontend/booking.js
+++ lwc/frontend/booking.js
@@ -1,8 +1,9 @@
-import './common/common.scss';
-import '@lwc/synthetic-shadow';
-import { createElement } from 'lwc';
-import Admin from './components/admin/admin';
+import "./common/common.scss";
+import "@lwc/synthetic-shadow";
+import { createElement } from "lwc";
+import Admin from "./components/admin/admin";
 
-const appEl = createElement('z-admin', { is: Admin });
+const appEl = createElement("z-admin", { is: Admin });
 appEl.model = JSON.parse(model);
+console.log(appEl.model);
 document.body.appendChild(appEl);
