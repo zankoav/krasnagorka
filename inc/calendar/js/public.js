878406f892f1a778fab40437ef530903983a9e21
Alexandr <aleksandr.zanko@enway.com>
2020-05-23 11:03:32 +0300

    update

--- inc/calendar/js/public.js
+++ inc/calendar/js/public.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function ($) {
         let cButtons = document.querySelectorAll(".booking-houses__calendars-button");
         for await (let button of cButtons) {
             $(button).trigger("click");
-            await new Promise(resolve => { setTimeout(resolve, 1000) });
+            await new Promise(resolve => { setTimeout(resolve, 2000) });
             // index++;
         }
     }
