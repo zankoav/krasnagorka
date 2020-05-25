a6119cb05b493eae178e69eb7a9ca9300dd511b8
Alexandr <aleksandr.zanko@enway.com>
2020-05-25 22:25:30 +0300

    gert

--- mastak/core/custom_types.php
+++ mastak/core/custom_types.php
@@ -1818,28 +1818,28 @@
         return $house;
     }
 
-    function cmb2_get_term_options( $field ) {
-        $args = $field->args( 'get_terms_args' );
-        $args = is_array( $args ) ? $args : array();
+    // function cmb2_get_term_options( $field ) {
+    //     $args = $field->args( 'get_terms_args' );
+    //     $args = is_array( $args ) ? $args : array();
     
-        $args = wp_parse_args( $args, array( 'taxonomy' => 'category' ) );
+    //     $args = wp_parse_args( $args, array( 'taxonomy' => 'category' ) );
     
-        $taxonomy = $args['taxonomy'];
+    //     $taxonomy = $args['taxonomy'];
     
-        $terms = (array) cmb2_utils()->wp_at_least( '4.5.0' )
-            ? get_terms( $args )
-            : get_terms( $taxonomy, $args );
+    //     $terms = (array) cmb2_utils()->wp_at_least( '4.5.0' )
+    //         ? get_terms( $args )
+    //         : get_terms( $taxonomy, $args );
     
-        // Initate an empty array
-        $term_options = array();
-        if ( ! empty( $terms ) ) {
-            foreach ( $terms as $term ) {
-                $term_options[ $term->term_id ] = $term->name;
-            }
-        }
+    //     // Initate an empty array
+    //     $term_options = array();
+    //     if ( ! empty( $terms ) ) {
+    //         foreach ( $terms as $term ) {
+    //             $term_options[ $term->term_id ] = $term->name;
+    //         }
+    //     }
     
-        return $term_options;
-    }
+    //     return $term_options;
+    // }
 
     function show_event_options() {
 
