320186fcbc8d8c0e12ccfe185029d11e7c0a0870
Alexandr <aleksandr.zanko@enway.com>
2020-06-24 15:54:44 +0300

    wegbb

---
+++ page-zanko.php
@@ -0,0 +1,19 @@
+<?php
+    if (!defined('ABSPATH')) {
+        exit;
+    }?>
+
+
+    <h1>Hello Alexandr</h1>
+    <p>Testing ...</p>
+
+    <?php
+        $clientId = 'ivan.6113446@gmail.com';
+        $clientSecret = '20b87286dec1a72badab4db93cfef117fb9fedf6';
+        $redirectUri = 'krasnogorka';
+        $apiClient = new \AmoCRM\Client\AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+
+        var_dump($apiClient);
+
+    
+    ?>
