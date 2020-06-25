55a6f5700fc202fbca264b4e8d26313cf8a51615
Alexandr <aleksandr.zanko@enway.com>
2020-06-25 09:56:47 +0300

    fewreb

--- page-zanko.php
+++ page-zanko.php
@@ -14,14 +14,14 @@
     <?php
         $clientId = 'ivan.6113446@gmail.com';
         $clientSecret = '20b87286dec1a72badab4db93cfef117fb9fedf6';
-        $redirectUri = 'https://krasnogorka.amocrm.ru/private/api/auth.php';
+        $redirectUri = 'krasnogorka.amocrm.ru';
 
         if ( ! class_exists( '\AmoCRM\Client\AmoCRMApiClient' ) ) 
             return;
 
         $apiClient = new \AmoCRM\Client\AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
-        $apiClient
-            ->setAccountBaseDomain('krasnogorka.amocrm.ru');
+        // $apiClient
+        //     ->setAccountBaseDomain('krasnogorka.amocrm.ru');
         //     ->setAccessToken(new \League\OAuth2\Client\Token\AccessToken([
         //         'access_token' => '123456',
         //         'refresh_token' => '123456',
