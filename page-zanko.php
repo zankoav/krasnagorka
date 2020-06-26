957dc3cd156005bc8e0be4565c1ad9bd563681f0
Alexandr <aleksandr.zanko@enway.com>
2020-06-26 22:27:56 +0300

    qwegwbwe

--- page-zanko.php
+++ page-zanko.php
@@ -90,7 +90,7 @@
         );
         
         try {
-            // $lead = $leadsService->addOne($lead);
+            $lead = $leadsService->addOne($lead);
             //  var_dump($lead);
         } catch (AmoCRMApiException $e) {
             echo '<pre>',$e->getTitle(),$e->getDescription(),'</pre>';
@@ -183,7 +183,7 @@
 
                     echo '<pre>', 'OK 4', '</pre>';
                     // var_dump($contact);
-                    // $contact = $apiClient->contacts()->addOne($contact);
+                    $contact = $apiClient->contacts()->addOne($contact);
                     echo '<pre>', 'OK 5', '</pre>';
 
                 } catch (AmoCRMApiException $e) {
