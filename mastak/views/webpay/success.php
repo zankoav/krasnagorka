74c04f721902fe2b17641c9b6db82a9c0f707ea8
Alexandr <aleksandr.zanko@enway.com>
2020-10-25 15:11:28 +0300

    rdtyfugu

--- mastak/views/webpay/success.php
+++ mastak/views/webpay/success.php
@@ -1,10 +1,26 @@
 <?php 
     $order = $args['order'];
 ?>
-<table>
+<style>
+    #check{
+        margin-left: auto; 
+        margin-right: auto;
+        color:  #333;
+        width: 400px;
+        font-size: 16px;
+        border: 1px solid #ddd;
+        border-radius: 16px;
+    }
+    #check th, #check td {
+        border: 1px solid #ddd;
+        padding: 12px;
+        text-align: left;
+    }
+</style>
+<table id="check">
     <tbody>
         <tr>
-            <td colspan="2">Информация о бронировании</td>
+            <td colspan="2" style="text-align:center;text-transform:uppercase;font-size:18px;">Информация о бронировании</td>
         </tr>
         <tr>
             <td>Домик</td>
