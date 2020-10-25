9967a7813a320c5a5b151f2ddbb8ac2cc9f3939c
Alexandr <aleksandr.zanko@enway.com>
2020-10-25 16:24:50 +0300

    fsdgg

--- mastak/views/webpay/success.php
+++ mastak/views/webpay/success.php
@@ -2,77 +2,87 @@
     $order = $args['order'];
 ?>
 <style>
-    #check{
-        margin-left: auto; 
-        margin-right: auto;
-        color:  #333;
-        width: 500px;
-        font-size: 16px;
-        border: 1px solid #ddd;
-        border-radius: 8px;
-        border-spacing: 0;
-        background-color: #fafafa;
-    }
-
     @media (max-width:768px){
         #check{
-            width: 100%;
-            font-size: 14px;
+            width: 100% !important;
+            font-size: 14px !important;
         }
     }
-
-    #check tr:first-child td{
-        background: #1498c6;
-        color: #fff;
-        font-weight: 600;
-        border-top-left-radius: 8px;
-        border-top-right-radius: 8px;
-    }
-
-    #check td:last-child{
-        font-weight: 600;
-    }
-
-    #check td {
-        border-bottom: 1px solid #ddd;
-        padding: 12px;
-        text-align: left;
-        color: #555;
-    }
-
-    #check tr:last-child td {
-        border-bottom: none;
-    }
-
-    #check tr:last-child td:first-child {
-        border-bottom-left-radius: 12px;
-    }
-    
-    #check tr:last-child td:last-child {
-        border-bottom-right-radius: 12px;
-    }
-
 </style>
-<table id="check">
+<table id="check" style="
+    margin-left:auto;
+    margin-right:auto;
+    color:  #333;
+    width: 500px;
+    font-size: 16px;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    border-spacing: 0;
+    background-color: #fafafa;">
     <tbody>
         <tr>
-            <td colspan="2" style="text-align:center;text-transform:uppercase;font-size:18px;">Информация о бронировании</td>
+            <td colspan="2" style="
+                text-align:center;
+                text-transform:uppercase;
+                font-size:18px;
+                background:#1498c6;
+                color:#fff;
+                font-weight:600;
+                border-top-left-radius:8px;
+                border-top-right-radius:8px;">Информация о бронировании</td>
         </tr>
         <tr>
-            <td>Домик</td>
-            <td><?=$order['calendar']?></td>
+            <td style="
+                border-bottom: 1px solid #ddd;
+                padding: 12px;
+                text-align: left;
+                color: #555;">Домик</td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                border-bottom: 1px solid #ddd;
+                font-weight:600;"><?=$order['calendar']?></td>
         </tr>
         <tr>
-            <td>Дата заезда</td>
-            <td><?=$order['from']?></td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                border-bottom: 1px solid #ddd;">Дата заезда</td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                font-weight:600;
+                border-bottom: 1px solid #ddd;"><?=$order['from']?></td>
         </tr>
         <tr>
-            <td>Дата выезда</td>
-            <td><?=$order['to']?></td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                border-bottom: 1px solid #ddd;">Дата выезда</td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                font-weight:600;"><?=$order['to']?></td>
         </tr>
         <tr>
-            <td>Оплачено</td>
-            <td><?=$order['price']?> руб.</td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                border-bottom:none;
+                border-bottom-left-radius:8px;">Оплачено</td>
+            <td style="
+                padding: 12px;
+                text-align: left;
+                color: #555;
+                font-weight:600;
+                border-bottom:none;
+                border-bottom-right-radius:8px;"><?=$order['price']?> руб.</td>
         </tr>
     </tbody>
 </table>
