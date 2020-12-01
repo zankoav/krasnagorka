62dea485ca1fcec4e46a56ae0d445a58be39cefa
Alexandr <aleksandr.zanko@enway.com>
2020-12-01 14:54:29 +0300

    34t5

--- rest/rest.php
+++ rest/rest.php
@@ -1,6 +1,8 @@
 <?php
     require __DIR__ . '/Booking_Form_Controller.php';
+    require __DIR__ . '/AmoCRM_Controller.php';
 
     add_action('rest_api_init', function () {
         (new Booking_Form_Controller())->register_routes();
+        (new AmoCRM_Controller())->register_routes();
     });
