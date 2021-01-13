cd7ea77d20049d1353af05a79022ab656dd6cbc3
lescha19891 <lescha19891@tut.by>
2021-01-14 00:06:35 +0300

    new  template

---
+++ template-LS главная.php
@@ -0,0 +1,25 @@
+<?php
+/**
+     *
+     * Template Name: LS Главная
+     *
+     */
+    
+
+?>
+<!DOCTYPE html>
+<head>   
+    <title> LS главная </title>
+</head>
+<body>      
+    <div style="color: rgb(0, 0, 255); font-size: 2em">
+        Шапка сайта
+    </div>    
+    <div style="color: rgb(0, 110, 255); font-size: 2em"> 
+        Контент
+    </div>
+    <div style="color: rgb(0, 0, 150); font-size: 2em">
+        Подвал
+    </div>
+</body>
+</html>
