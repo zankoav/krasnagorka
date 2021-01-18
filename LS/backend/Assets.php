9b7838f23c73d9540e9dd3038ec068481f8a55af
lescha19891 <lescha19891@tut.by>
2021-01-18 20:19:48 +0300

    modyfy Assets

--- LS/backend/Assets.php
+++ LS/backend/Assets.php
@@ -1,21 +1,21 @@
 <?php
     class Assets {
 
-        private static $assets;
+        private static $assets=null;
 
-        private static function set_assets() {
-            $path         = get_template_directory_uri() . "/src/assets.json";
+        private static function setAssets() {
+            if (self::$assets==null){
+            $path         = get_template_directory_uri() . "/LS/frontend/src/assets.json";
             $content      = file_get_contents($path);
             self::$assets = json_decode($content, true);
+            }
         }
-
         public function css($name) {
-            self:: set_assets();
+            self::setAssets();
             return self::$assets[$name]['css'];
         }
-
         public function js($name) {
-            self:: set_assets();
+            self::setAsets();
             return self::$assets[$name]['js'];
         }
     }
