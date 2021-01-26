04bc2a4f4664d2576edaab8793a6a8d6eb4f9795
lescha19891 <lescha19891@tut.by>
2021-01-26 22:47:39 +0300

    add  checkDevice,  plug style and script

--- LS/backend/Assets.php
+++ LS/backend/Assets.php
@@ -2,7 +2,16 @@
     class Assets {
 
         private static $assets=null;
-
+        private static $name=null;
+        private static function checkDevice() {
+            if (self::$assets==null){
+                if (wp_is_mobile()){
+                    return self::$name="main_mobile";
+                }   else {
+                    return self::$name="main_desktop";
+                }  
+            }     
+        }
         private static function setAssets() {
             if (self::$assets==null){
             $path         = get_template_directory_uri() . "/LS/frontend/src/assets.json";
@@ -10,12 +19,14 @@
             self::$assets = json_decode($content, true);
             }
         }
-        public function css($name) {
+        public function css() {
+            self::checkDevice();
             self::setAssets();
-            return self::$assets[$name]['css'];
+            return self::$assets[self::$name]['css'];
         }
-        public function js($name) {
-            self::setAsets();
-            return self::$assets[$name]['js'];
+        public function js() {
+            self::checkDevice();
+            self::setAssets();
+            return self::$assets[self::$name]['js'];
         }
     }
--- template-ls-primary.php
+++ template-ls-primary.php
@@ -5,12 +5,13 @@
      *
      */
     
-
+     require_once __DIR__ . '/LS/backend/Assets.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>   
         <title> LS главная </title>
+        <link rel="stylesheet" href="<?=Assets::css()?>">
     </head>
     <body>      
         <div style="color: rgb(0, 0, 255); font-size: 2em">
@@ -22,5 +23,6 @@
         <div style="color: rgb(0, 0, 150); font-size: 2em">
             Подвал
         </div>
+        <script type="text/javascript"  src="<?=Assets::js()?>"></script>
     </body>
 </html>
