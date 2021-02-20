c26713b1f0f0726d12e1921be77b61a3d493fdba
zankoav <zankoav@mail.ru>
2021-02-20 21:52:12 +0300

    update content

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -1,44 +1,49 @@
 <?php
-class LS_Booking_Form_Controller extends WP_REST_Controller{
-    
-    public function register_routes(){
+class LS_Booking_Form_Controller extends WP_REST_Controller
+{
+
+    public function register_routes()
+    {
         $namespace = 'krasnagorka/v1';
         $path      = '/ls/house/';
-        
+
         register_rest_route($namespace, $path, [
             array(
                 'methods'             => 'GET',
-                'callback'            => array($this, 'order_house')                
+                'callback'            => array($this, 'order_house')
             ),
         ]);
     }
-    public function order_house($request){
-        $query = new WP_Query([ 
+    public function order_house($request)
+    {
+        $query = new WP_Query([
             'post_type' => 'house',
             'posts_per_page' => -1
-        ]);   
-        $calendarId = trim($request['calendarId']);   
-        if  ($calendarId==''){
-            exit ('enter calendarID');
+        ]);
+        $calendarId = trim($request['calendarId']);
+        if ($calendarId == '') {
+            exit('enter calendarID');
         }
-        $houseInfo=[];
-        foreach ($query as $house ) {
+        $houseInfo = [];
+        foreach ($query as $house) {
             $query->the_post();
-            $metaId=get_post_meta(get_the_ID(), "mastak_house_calendar", true);
-            $metaId=preg_replace ('/[^0-9]/','',$metaId);
-            if  ($metaId==$calendarId){
+            $metaId = get_post_meta(get_the_ID(), "mastak_house_calendar", true);
+            $metaId = preg_replace('/[^0-9]/', '', $metaId);
+            if ($metaId == $calendarId) {
                 $imageId = get_post_thumbnail_id();
-                $picture=wp_get_attachment_image_url($imageId, 'header_iphone_5');
-                $houseInfo=[
-                    'id'=>get_the_ID(),
-                    'description'=>get_post_meta(get_the_ID(), "mastak_house_small_description", true),
-                    'peopleMaxCount'=>get_post_meta(get_the_ID(), "max_people", true),
-                    'picture'=>$picture,
-                    'title'=>get_the_title()
+                $picture = wp_get_attachment_image_url($imageId, 'header_iphone_5');
+                $houseInfo = [
+                    'id' => get_the_ID(),
+                    'description' => get_the_content(),
+                    'peopleMaxCount' => get_post_meta(get_the_ID(), "max_people", true),
+                    'picture' => $picture,
+                    'title' => get_the_title()
                 ];
                 break;
-            }  else{$houseInfo='this house not found';} 
-        }      
+            } else {
+                $houseInfo = 'this house not found';
+            }
+        }
         return new WP_REST_Response($houseInfo, 200);  // addres wordpress/wp-json/krasnagorka/v1/ls/house/?calendarId=5
-    }    
-}   
+    }
+}
