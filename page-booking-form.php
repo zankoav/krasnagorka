195e603d5431f78dc15660cba231958fcf7225cb
zankoav <zankoav@mail.ru>
2021-02-23 19:55:00 +0300

    Update

--- page-booking-form.php
+++ page-booking-form.php
@@ -15,6 +15,7 @@ $bookingModel = $model->getBookingModel();
     <link rel="shortcut icon" href="<?= get_site_icon_url(); ?>" type="image/x-icon">
     <title><?= get_the_title(); ?></title>
     <link rel="stylesheet" href="<?= $assets->css('booking'); ?>">
+    <link href="https://krasnagorka.by/wp-content/themes/krasnagorka/inc/calendar/css/public_style.css" rel="stylesheet"/>
 </head>
 
 <body>
@@ -37,6 +38,11 @@ $bookingModel = $model->getBookingModel();
 
         const model = `<?= $bookingModel; ?>`;
     </script>
+    <script type='text/javascript' src='https://krasnagorka.by/wp-includes/js/jquery/jquery.min.js' id='jquery-core-js'></script>
+    <script type='text/javascript' src='https://krasnagorka.by/wp-includes/js/jquery/jquery-migrate.min.js' id='jquery-migrate-js'></script>
+    <script type='text/javascript' src='https://krasnagorka.by/wp-includes/js/dist/vendor/moment.min.js' id='moment-js'></script>
+    <script type='text/javascript' src='https://krasnagorka.by/wp-content/themes/krasnagorka/inc/calendar/js/fullcalendar.min.js' id='fullcalendar-js'></script>
+    <script type='text/javascript' src='https://krasnagorka.by/wp-content/themes/krasnagorka/inc/calendar/js/ru.js' id='ru-js'></script>
     <script src="<?= $assets->js('booking'); ?>"></script>
 </body>
 
