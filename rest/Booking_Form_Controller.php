d9c8e366f8176327193935071c4a9113ab0c8c41
zankoav <zankoav@mail.ru>
2021-02-25 19:17:34 +0300

    update

--- rest/Booking_Form_Controller.php
+++ rest/Booking_Form_Controller.php
@@ -651,7 +651,7 @@ class Booking_Form_Controller extends WP_REST_Controller
                         'wsb_invoice_item_price[0]' => $wsb_total,
                         'wsb_total' => $wsb_total,
                         'wsb_notify_url' => 'https://krasnagorka.by/wp-json/krasnagorka/v1/pay-success/',
-                        'wsb_return_url' => "https://krasnagorka.by/payed-success",
+                        // 'wsb_return_url' => "https://krasnagorka.by/payed-success",
                     ]
                 ];
                 $resultStatus = 200;
@@ -671,13 +671,13 @@ class Booking_Form_Controller extends WP_REST_Controller
         generateCheck($_POST['site_order_id']);
         $checkOutList = ob_get_contents();
         ob_end_clean();
-        wp_mail(
-            [
-                $order['email']
-            ],
-            'Успешная оплата в Красногорке',
-            $checkOutList
-        );
+        // wp_mail(
+        //     [
+        //         $order['email']
+        //     ],
+        //     'Успешная оплата в Красногорке',
+        //     $checkOutList
+        // );
         if ($_POST['transaction_id']) {
             update_post_meta($_POST['site_order_id'], 'sbc_order_select', 'booked');
             update_post_meta($_POST['site_order_id'], 'sbc_order_prepaid', $order['price']);
@@ -729,7 +729,7 @@ class Booking_Form_Controller extends WP_REST_Controller
             try {
                 $task = $apiClient->tasks()->getOne($taskId);
                 $task->setTaskTypeId(925198)
-                    ->setText('Клиент оплатил 100%')
+                    ->setText('Клиент оплатил 100%. Отправить чек клиенту.')
                     ->setCompleteTill(mktime(date("H"), date("i") + 30))
                     ->setEntityType(EntityTypesInterface::LEADS)
                     ->setEntityId($lead->getId())
