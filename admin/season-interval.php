7f1b60978aab58a885ec0286b7e613ff758d5791
zankoav <zankoav@mail.ru>
2021-04-05 17:26:05 +0300

    kjn

--- admin/season-interval.php
+++ admin/season-interval.php
@@ -5,7 +5,7 @@ if(isset($_POST['season-generator'], $_POST['from'], $_POST['to'])){
 ?>
 <div class="wrap">
     <h1 class="wp-heading">Seasons generator</h1>
-    <form action="" action="POST">
+    <form action="" method="POST">
         <input type="date" name="from" readonly/>
         <input type="date" name="to" readonly/>
         <input type="submit" name="season-generator" value="Create"/>
