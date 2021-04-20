f93dbe7b80555e534ceed545521d83f839e1aef0
zankoav <zankoav@mail.ru>
2021-04-20 22:21:05 +0300

    ауццуа

--- mastak/core/cmb2/season.php
+++ mastak/core/cmb2/season.php
@@ -104,6 +104,57 @@ function register_post_type_team_member()
 			'query_var'           => true,
 		)
 	);
+
+	register_post_type(
+		'sale',
+		array(
+			'label'               => null,
+			'labels'              => array(
+				'name'               => 'Cкидка', // основное название для типа записи
+				'singular_name'      => 'Cкидка', // название для одной записи этого типа
+				'add_new'            => 'Добавить Cкидку', // для добавления новой записи
+				'add_new_item'       => 'Добавить Cкидку', // заголовка у вновь создаваемой записи в админ-панели.
+				'edit_item'          => 'Редактировать Cкидку', // для редактирования типа записи
+				'new_item'           => 'Новая Cкидка', // текст новой записи
+				'view_item'          => 'Посмотреть Cкидку', // для просмотра записи этого типа.
+				'search_items'       => 'Искать Cкидку', // для поиска по этим типам записи
+				'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+				'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+				'parent_item_colon'  => '', // для родителей (у древовидных типов)
+				'menu_name'          => 'Cкидки', // название меню
+			),
+			'description'         => '',
+			'public'              => false,
+			'publicly_queryable'  => true,
+			// зависит от public
+			'exclude_from_search' => true,
+			// зависит от public
+			'show_ui'             => true,
+			// зависит от public
+			'show_in_menu'        => null,
+			// показывать ли в меню адмнки
+			'show_in_admin_bar'   => null,
+			// по умолчанию значение show_in_menu
+			'show_in_nav_menus'   => false,
+			// зависит от public
+			'show_in_rest'        => null,
+			// добавить в REST API. C WP 4.7
+			//    'rest_base'           => null, // $post_type. C WP 4.7
+			'menu_position'       => 7,
+			//    'menu_icon'           => null,
+			'menu_icon'           => 'dashicons-buddicons-groups',
+			//'capability_type'   => 'post',
+			//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+			//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+			//'hierarchical'        => false,
+			'supports'            => ['page-attributes'],
+			// 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+			//'taxonomies'          => array(),
+			'has_archive'         => false,
+			'rewrite'             => false,
+			'query_var'           => true,
+		)
+	);
 }
 
 add_action('cmb2_admin_init', 'mastak_season');
@@ -124,6 +175,15 @@ function mastak_season()
 			'id'   =>  'house_price_' . $house_id,
 			'type'         => 'text_money',
 			'before_field' => 'BYN'
+		));
+		$cmb_season->add_field(array(
+			'name' => 'Минимальное кол-во людей',
+			'id'   =>  'house_min_people_' . $house_id,
+			'type'            => 'text',
+			'attributes'      => array(
+				'type'    => 'number',
+				'min' => '1'
+			)
 		));
 	}
 
@@ -183,6 +243,62 @@ function mastak_season_interval()
 		),
 		'display_cb' => 'season_display_cb',
 		'escape_cb' => 'season_escape_cb',
+	));
+}
+
+
+add_action('cmb2_admin_init', 'mastak_sales');
+
+function mastak_sales()
+{
+
+	$cmb_sale = new_cmb2_box(array(
+		'id'           => 'sale_option',
+		'title'        => esc_html__('Локализация скидки', 'krasnagorka'),
+		'object_types' => array('sale'), // Post type
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Домик',
+		'id'            => 'sale_house',
+		'type' 			=> 'text'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Сезон',
+		'id'            => 'sale_season',
+		'type' 			=> 'text'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Настройки скидки',
+		'id'            => 'settings_title_id',
+		'type' 			=> 'title'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Человек >=',
+		'id'            => 'sale_people_min',
+		'type' 			=> 'text'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Дней >=',
+		'id'            => 'sale_days_min',
+		'type' 			=> 'text'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Процент',
+		'id'            => 'sale_percentage',
+		'type' 			=> 'text_money',
+		'before_field' => '%'
+	));
+
+	$cmb_sale->add_field(array(
+		'name'          => 'Подсказка о скидке',
+		'id'            => 'sale_help_message',
+		'type' 			=> 'textarea_small'
 	));
 }
 
