0391c718c2cac8ea005105bbf1a555c9063edb4b
aleksandr zanko <aleksandr.zanko@enway.com>
2021-08-08 19:11:03 +0300

    ewvwev

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -97,6 +97,45 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
         $dateEnd = $request['dateEnd'];
         $peopleCount = $request['peopleCount'];
 
+        $dateEndDT = new DateTime($dateEnd);
+
+        $period = new DatePeriod(
+            new DateTime($dateStart),
+            new DateInterval('P1D'),
+            $dateEndDT->modify( '+1 day' )
+        );
+
+        $days = [];
+        foreach ($period as $key => $value) {
+            $days[] = $value->format('Y-m-d');    
+        }
+
+        $houseDaysSales = get_post_meta($houseId, 'sale_days', 1);
+        $houseDaysSalesResult = [];
+        foreach ((array)$houseDaysSales as $key => $entry) {
+            if (isset($entry['sale']) and isset($entry['dayes'])) {
+                $houseDaysSalesResult[$entry['dayes']] = $entry['sale'];
+            }
+        }
+        ksort($houseDaysSalesResult);
+
+        $daysSale = null;
+        $daySaleNext = null;
+        
+        foreach($houseDaysSalesResult as $dayesNumber => $sale){
+            $daySaleNext = [
+                'sale' => $sale,
+                'dayesNumber' => $dayesNumber
+            ];
+            if(count($days) < $dayesNumber){
+                break;
+            }else{
+                $daysSale = $sale;
+                $daySaleNext = null;
+            }
+        }
+
+
         $intervals = $this->firstCalculeate($dateStart, $dateEnd);
 
         if(count($intervals) == 2){
@@ -117,21 +156,9 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
             ];
         }
 
-        $dateEndDT = new DateTime($dateEnd);
-
-        $period = new DatePeriod(
-            new DateTime($dateStart),
-            new DateInterval('P1D'),
-            $dateEndDT->modify( '+1 day' )
-        );
-
-        $days = [];
-        foreach ($period as $key => $value) {
-            $days[] = $value->format('Y-m-d');    
-        }
-
         $result = [
             'days_count' => count($days),
+            'day_sale_next' => $daySaleNext,
             'seasons_group' => []
         ];
 
@@ -159,30 +186,71 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
 
         $seasons   = $seasonsQuery->get_posts();
         foreach ($seasons as $season) {
-            $housePrice = get_post_meta($season->ID, 'house_price_'.$houseId, 1);
-            $houseMinPeople = get_post_meta($season->ID, 'house_min_people_'.$houseId, 1);
-            $houseMinDays = get_post_meta($season->ID, 'house_min_days_'.$houseId, 1);
-            $houseMinPercent = get_post_meta($season->ID, 'house_min_percent_'.$houseId, 1);
+            $housePrice = (float)get_post_meta($season->ID, 'house_price_'.$houseId, 1);
+            $houseMinPeople = (float)get_post_meta($season->ID, 'house_min_people_'.$houseId, 1);
+            $houseMinDays = (float)get_post_meta($season->ID, 'house_min_days_'.$houseId, 1);
+            $houseMinPercent = (float)get_post_meta($season->ID, 'house_min_percent_'.$houseId, 1);
 
             $result['seasons_group'][$season->ID]['house_price'] = $housePrice;
             $result['seasons_group'][$season->ID]['house_min_people'] = $houseMinPeople;
             $result['seasons_group'][$season->ID]['house_min_days'] = $houseMinDays;
             $result['seasons_group'][$season->ID]['house_min_percent'] = $houseMinPercent;
 
+            $basePrice = $housePrice;
+
+            $seasonDaysCount = count($result['seasons_group'][$season->ID]['days']);
+        
+            if(!empty($houseMinDays) && !empty($houseMinPercent) && ($seasonDaysCount < $houseMinDays)){
+                $basePrice *= (1 + (float)$houseMinPercent / 100);
+            }
+
+            $basePeopleCount = (int)$peopleCount;
+
+            if($peopleCount < $houseMinPeople){
+                $basePrice =  (float)$housePrice * (float)$houseMinPeople;
+                $basePeopleCount = null;
+            }
+
+            $result['seasons_group'][$season->ID]['price_block'] = [
+                'title' =>  $season->post_title,
+                'base_price' => $basePrice,
+                'days_count' => $seasonDaysCount,
+                'base_people_count' => $basePeopleCount,
+                'days_sale' => $daysSale
+            ];
+
             $housePeoplesForSalesEntities = get_post_meta($season->ID, 'house_days_for_sale_'.$houseId, 1);
             $housePeoplesForSales = [];
 
             foreach ((array)$housePeoplesForSalesEntities as $key => $entry) {
-                $housePeoplesForSale = [];
-
                 if (isset($entry['sale_percent']) and isset($entry['sale_days'])) {
-                    $housePeoplesForSale['people'] = $entry['sale_days'];
-                    $housePeoplesForSale['percent'] = $entry['sale_percent'];
-                    $housePeoplesForSales[] = $housePeoplesForSale;
+                    $housePeoplesForSales[$entry['sale_days']] = $entry['sale_percent'];
                 }
             }
 
-            $result['seasons_group'][$season->ID]['peoples_for_sales'] = $housePeoplesForSales;
+            ksort($housePeoplesForSales);
+
+            $peopleSale = null;
+            $peopleSaleNext = null;
+            
+            foreach($housePeoplesForSales as $peopleNumber => $sale){
+                
+                $peopleSaleNext = [
+                    'sale' => $sale,
+                    'people' => $peopleNumber
+                ];
+
+                if(count($peopleCount) < $peopleNumber){
+                    break;
+                }else{
+                    $peopleSale = $sale;
+                    $peopleSaleNext = null;
+                }
+            }
+
+            $result['seasons_group'][$season->ID]['price_block']['people_sale'] = $peopleSale;
+            $result['seasons_group'][$season->ID]['price_block']['people_sale_next'] = $peopleSaleNext;
+
         }
         
         return new WP_REST_Response( $result, 200);
