88897b0760778f201f866e25e109f9675a9452eb
aleksandr zanko <aleksandr.zanko@enway.com>
2021-08-08 20:11:49 +0300

    update

--- lwc/frontend/components/decodingPrice/decodingPrice.js
+++ lwc/frontend/components/decodingPrice/decodingPrice.js
@@ -3,4 +3,8 @@ import './decodingPrice.scss';
 
 export default class DecodingPrice extends LightningElement {
 	@api settings;
+
+	get seasons (){
+		return Object.values(this.settings.total.seasons_group);
+	}
 }
