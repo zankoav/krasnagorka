d51f7a01beb5dfcce64ccd021d1b45eba8ef5fa4
aleksandr zanko <aleksandr.zanko@enway.com>
2021-08-18 18:33:55 +0300

    wevwev

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -115,6 +115,9 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
             $days[] = $value->format('Y-m-d');    
         }
 
+        LS_WP_Logger::info('days: ' . json_encode( $days));
+
+
         $houseDaysSales = get_post_meta($houseId, 'sale_days', 1);
         $houseDaysSalesResult = [];
         foreach ((array)$houseDaysSales as $key => $entry) {
