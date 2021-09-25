9fead12484bc077dae9f955314a6e1d84863d69d
aleksandr zanko <aleksandr.zanko@enway.com>
2021-09-25 16:32:13 +0300

    uuu

--- L-S/cmb2/order-payment.php
+++ L-S/cmb2/order-payment.php
@@ -29,30 +29,14 @@ function payment_metabox() {
         'name' => 'Оплата',
         'id'   => 'sbc_order_prepaid_percantage',
         'type'         => 'text_small',
-        'before_field' => '%',
-        'attributes' => array(
-            'readonly' => 'readonly'
-        )
+        'before_field' => '%'
     ));
 
     $sbc_client->add_field(array(
         'name' => 'Ссылка на оплату',
         'desc' => 'Данная ссылка отправляется клиенту в случае оплаты по карте позже',
         'id'   => 'sbc_order_prepaid_link',
-        'type' => 'text_url',
-        'attributes' => array(
-            'readonly' => 'readonly'
-        )
-    ));
-
-    $sbc_client->add_field(array(
-        'name' => 'Ссылка на оплату',
-        'desc' => 'Данная ссылка отправляется клиенту в случае оплаты по карте позже',
-        'id'   => 'sbc_order_prepaid_link',
-        'type' => 'text_url',
-        'attributes' => array(
-            'readonly' => 'readonly'
-        )
+        'type' => 'text_url'
     ));
 
     $sbc_client->add_field(array(
