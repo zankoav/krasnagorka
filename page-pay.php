df96457aa7df4e8f6e925d8c17dff54edf978be7
aleksandr zanko <aleksandr.zanko@enway.com>
2021-09-26 14:12:44 +0300

    fewgrt

--- page-pay.php
+++ page-pay.php
@@ -2,9 +2,17 @@
     if (!defined('ABSPATH')) {
         exit;
     }
+
     $source = $_GET['source'];
+    if(empty( $source)){
+        exit;
+    }
+
+    $args = array(
+        'meta_key'   => 'sbc_order_prepaid_source',
+        'meta_value' => $source
+    );
+    $query = new WP_Query( $args );
+    var_dump($query);
 
 ?>
-<?php if(!empty($source)):?>
-    <p>Source: <?=$source?></p>
-<?php endif;?>
