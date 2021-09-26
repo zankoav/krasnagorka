3bc7a583bfec4fcb67c662a74dd9ea7ccd990cc4
aleksandr zanko <aleksandr.zanko@enway.com>
2021-09-26 14:39:02 +0300

    efwgrt

--- page-pay.php
+++ page-pay.php
@@ -26,7 +26,6 @@
     while ( $query->have_posts() ) {
         $query->the_post();
         $ID = get_the_ID();
-        var_dump('OK', $ID);
         break;
     }
 
@@ -42,5 +41,33 @@
 
 <script>
     const params = <?=$formData?>;
-    console.log('params', params);
+    const paymentOrg = false ? 'https://securesandbox.webpay.by' : 'https://payment.webpay.by'
+    generateAndSubmitForm(paymentOrg, params.values, params.names);
+
+    function generateAndSubmitForm(action, paramsWithValue, paramsWithNames, method = 'POST') {
+        const form = document.createElement("form");
+        form.action = action;
+        form.method = method;
+
+        paramsWithValue.wsb_cancel_return_url = `${location.href}&clear=${paramsWithValue.wsb_order_num}`;
+
+        // eslint-disable-next-line guard-for-in
+        for (const key in paramsWithValue) {
+            const element = document.createElement("input");
+            element.type = "hidden";
+            element.name = key;
+            element.value = paramsWithValue[key];
+            form.appendChild(element);
+        }
+
+        for (const key of paramsWithNames) {
+            const element = document.createElement("input");
+            element.type = "hidden";
+            element.name = key;
+            form.appendChild(element);
+        }
+
+        document.body.appendChild(form);
+        form.submit();
+    }
 </script>
