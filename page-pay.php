85654e52ee47c420e06e1ed64a8f3e62ed9c2510
aleksandr zanko <aleksandr.zanko@enway.com>
2021-09-26 14:42:06 +0300

    rgebt

--- page-pay.php
+++ page-pay.php
@@ -51,7 +51,6 @@
 
             paramsWithValue.wsb_cancel_return_url = `${location.href}&clear=${paramsWithValue.wsb_order_num}`;
 
-            // eslint-disable-next-line guard-for-in
             for (const key in paramsWithValue) {
                 const element = document.createElement("input");
                 element.type = "hidden";
