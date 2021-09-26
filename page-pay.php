ad72f5be9f05d94bbbab9a207541a687658ed56c
aleksandr zanko <aleksandr.zanko@enway.com>
2021-09-26 14:27:52 +0300

    evwf

--- page-pay.php
+++ page-pay.php
@@ -10,15 +10,28 @@
     if(empty( $source)){
         exit;
     }
+
     $args = array(
-        'meta_key'   => 'sbc_order_prepaid_source',
-        'meta_value' => $source
+        'post_type'  => 'sbc_order',
+        'meta_query' => array(
+            array(
+                'key'     => 'sbc_order_prepaid_source',
+                'value'   =>  $source,
+                'compare' => '=',
+            ),
+        ),
     );
+    // $query = new WP_Query( $args );
+    // $args = array(
+    //     'meta_key'   => 'sbc_order_prepaid_source',
+    //     'meta_value' => $source
+    // );
 
     $query = new WP_Query( $args );
     while ( $query->have_posts() ) {
         $query->the_post();
         $ID = get_the_ID();
+        var_dump('ID', $ID );
         break;
     }
 
