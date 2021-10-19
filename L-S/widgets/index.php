cf433d9d082f999486a83753c313e167344dca83
aleksandr zanko <aleksandr.zanko@enway.com>
2021-10-19 13:20:04 +0300

    hjb

--- L-S/widgets/index.php
+++ L-S/widgets/index.php
@@ -1,12 +1,10 @@
 <?php
 if (!defined('ABSPATH')) { exit; }
 
-if(is_admin()){
-    require __DIR__ . '/opportunities-page.php';
-    require __DIR__ . '/houses-page.php';
-    require __DIR__ . '/events-page.php';
-    require __DIR__ . '/price-page.php';
-    require __DIR__ . '/home-page.php';
-    require __DIR__ . '/place-page.php';
-    require __DIR__ . '/news-page.php';
-}
+require __DIR__ . '/opportunities-page.php';
+require __DIR__ . '/houses-page.php';
+require __DIR__ . '/events-page.php';
+require __DIR__ . '/price-page.php';
+require __DIR__ . '/home-page.php';
+require __DIR__ . '/place-page.php';
+require __DIR__ . '/news-page.php';
