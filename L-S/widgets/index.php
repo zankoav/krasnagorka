1df24e949fb00e4d0280f3339ac4e8ed5b82166e
aleksandr zanko <aleksandr.zanko@enway.com>
2021-10-19 13:15:04 +0300

    цуицуи

--- L-S/widgets/index.php
+++ L-S/widgets/index.php
@@ -8,4 +8,5 @@ if(is_admin()){
     require __DIR__ . '/price-page.php';
     require __DIR__ . '/home-page.php';
     require __DIR__ . '/place-page.php';
+    require __DIR__ . '/news-page.php';
 }
