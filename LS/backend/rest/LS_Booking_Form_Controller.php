73bcfc29b348e69b64309c6061d3c008840e992d
aleksandr zanko <aleksandr.zanko@enway.com>
2022-01-05 13:34:51 +0300

    ghjk

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -254,6 +254,11 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
             if(count($daysUpperPersents) > 0){    
                 foreach($daysUpperPersents as $day => $persent){
                     if($seasonDaysCount <= intval($day)){
+                        Log::info("Total days",$days);
+                        Log::info("seasonDaysCount",$seasonDaysCount);
+                        Log::info("day",$day);
+                        Log::info("persent",$persent);
+                        Log::info("===========",null);
                         $upperPercent = intval($persent);
                         break;
                     }
