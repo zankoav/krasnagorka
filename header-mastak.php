bd6172696b5214bd8dbe45438caa8c2875324e3a
aleksandr zanko <aleksandr.zanko@enway.com>
2022-01-15 13:08:01 +0300

    sdbf

--- header-mastak.php
+++ header-mastak.php
@@ -31,9 +31,6 @@ if (!defined('ABSPATH')) {
 	<link rel="preload" as="font" type="font/woff" crossorigin href="https://krasnagorka.by/wp-content/themes/krasnagorka/mastak/src/fonts/AvenirNextCyr-UltraLight.decf85.woff" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <?php if(is_page(9231)):?>
-        <meta name="robots" content="noindex, nofollow">
-    <?php endif;?>
 	<?php wp_head(); ?>
 	<style>
 		.modal-online-video {
