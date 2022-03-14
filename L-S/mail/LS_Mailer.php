3a0ebe0be868c2e093d9e88703df68ac8b876fc0
aleksandr zanko <aleksandr.zanko@enway.com>
2022-03-14 16:23:16 +0300

    Update

--- L-S/mail/LS_Mailer.php
+++ L-S/mail/LS_Mailer.php
@@ -40,6 +40,30 @@ class LS_Mailer {
     }
 
     private static function generateCheck($checkType, $data){
+        $babyBed = '';
+        $bathHouseWhite = '';
+        $bathHouseBlack = '';
+        if($data['babyBed']){
+            $babyBed = "<tr>
+            <td>Детская кроватка:</td>
+            <td class='f-b'>Да</td>
+            </tr>";
+        }
+
+        if(!empty($data['bathHouseWhite'])){
+            $bathHouseWhite = "<tr>
+            <td>Количество сеансов бани по-белому:</td>
+            <td class='f-b'>".$data['bathHouseWhite']."</td>
+            </tr>";
+        }
+
+        if(!empty($data['bathHouseBlack'])){
+            $bathHouseBlack = "<tr>
+            <td>Количество сеансов бани по-черному:</td>
+            <td class='f-b'>".$data['bathHouseBlack']."</td>
+            </tr>";
+        }
+
         $result = "<style type='text/css'>
         .title{padding-bottom: 8pt;padding-top: 12pt;font-size: 20pt;}
         .f-b{font-weight: bold;}
@@ -63,48 +87,51 @@ class LS_Mailer {
         </tr>
         <tr>
         <td>Заселение:</td>
-        <td class='f-b'>04.04.2021 14:00 – 22:00</td>
+        <td class='f-b'>".$data['from']." 14:00 – 22:00</td>
         </tr>
         <tr>
         <td>Выселение:</td>
-        <td class='f-b'>05.04.2021 09:00 – 12:00</td>
+        <td class='f-b'>".$data['to']." 09:00 – 12:00</td>
         </tr>
         <tr>
         <td colspan='2' class='title'>Информация об объектах размещения:</td>
         </tr>
         <tr>
         <td>Объект размещения:</td>
-        <td class='f-b'>Бабочка</td>
+        <td class='f-b'>".$data['calendarName']."</td>
         </tr>
         <tr>
         <td colspan='2' class='title'>Персональные данные клиента:</td>
         </tr>
         <tr>
         <td>ФИО:</td>
-        <td class='f-b'>Александр</td>
+        <td class='f-b'>".$data['fio']."</td>
         </tr>
         <tr>
         <td>Паспорт ID:</td>
-        <td class='f-b'>EGWEGWEHWE23523DDN</td>
+        <td class='f-b'>".$data['passport']."</td>
         </tr>
         <tr>
         <td>Контактный номер телефона:</td>
-        <td class='f-b'>+375296259983</td>
+        <td class='f-b'>".$data['phone']."</td>
         </tr>
         <tr>
         <td>Число гостей:</td>
-        <td class='f-b'>3</td>
+        <td class='f-b'>".$data['peopleCount']."</td>
         </tr>
+        ".$babyBed."
+        ".$$bathHouseWhite."
+        ".$$bathHouseBlack."
         <tr>
         <td colspan='2' class='title'>Оплата:</td>
         </tr>
         <tr>
         <td>Общая стоимость:</td>
-        <td class='f-b'>120 белорусских рублей.</td>
+        <td class='f-b'>".$data['price']." белорусских рублей.</td>
         </tr>
         <tr>
         <td>Предоплата внесена в размере:</td>
-        <td class='f-b'>120 белорусских рублей.</td>
+        <td class='f-b'>".$data['subprice']." белорусских рублей.</td>
         </tr>
         </tbody>
         </table>
