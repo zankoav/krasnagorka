0a88702e23dd6e941da7709b4f0cd28facf04878
aleksandr zanko <aleksandr.zanko@enway.com>
2022-03-14 11:40:58 +0300

    savd

--- L-S/mail/LS_Mailer.php
+++ L-S/mail/LS_Mailer.php
@@ -22,8 +22,8 @@ class LS_Mailer {
 
             $checkHTML = self::generateCheck($checkType, $data);
 
-            $html2pdf = new Html2Pdf('P', 'A4', 'ru', true);
-            $html2pdf->setDefaultFont('Arial');
+            $html2pdf = new Html2Pdf('P', 'A4', 'ru', true, 'UTF-8', 0);
+            // $html2pdf->setDefaultFont('Arial');
             try {
                     Log::info('1', '+');
                     $html2pdf->writeHTML($checkHTML);
@@ -43,86 +43,16 @@ class LS_Mailer {
     }
 
     private static function generateCheck($checkType, $data){
-        $result = `
-            <style type="text/Css">
-                .title {
-                    padding-bottom: 8pt;
-                    padding-top: 12pt;
-                    font-size: 20pt;
-                }
-                .f-b {
-                    font-weight: bold;
-                }
-            </style>
-            <page style="font-size: 13pt; font-family: Arial">
-                <table>
-                <tbody>
-                    <tr>
-                        <td colspan="2" class="title">ПОДТВЕРЖДЕНИЕ БРОНИРОВАНИЯ</td>
-                    </tr>
-                    <tr>
-                        <td>Номер бронирования:</td>
-                        <td class="f-b">28557</td>
-                    </tr>
-                    <tr>
-                        <td>от</td>
-                        <td class="f-b">25.02.2021</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="title">Ваши даты:</td>
-                    </tr>
-                    <tr>
-                        <td>Заселение:</td>
-                        <td class="f-b">04.04.2021 14:00 – 22:00</td>
-                    </tr>
-                    <tr>
-                        <td>Выселение:</td>
-                        <td class="f-b">05.04.2021 09:00 – 12:00</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="title">
-                            Информация об объектах размещения:
-                        </td>
-                    </tr>
-        
-                    <tr>
-                        <td>Объект размещения:</td>
-                        <td class="f-b">Бабочка</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="title">Персональные данные клиента:</td>
-                    </tr>
-                    <tr>
-                        <td>ФИО:</td>
-                        <td class="f-b">Александр</td>
-                    </tr>
-                    <tr>
-                        <td>Паспорт ID:</td>
-                        <td class="f-b">EGWEGWEHWE23523DDN</td>
-                    </tr>
-                    <tr>
-                        <td>Контактный номер телефона:</td>
-                        <td class="f-b">+375296259983</td>
-                    </tr>
-                    <tr>
-                        <td>Число гостей:</td>
-                        <td class="f-b">3</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="title">Оплата:</td>
-                    </tr>
-                    <tr>
-                        <td>Общая стоимость:</td>
-                        <td class="f-b">120 белорусских рублей.</td>
-                    </tr>
-                    <tr>
-                        <td>Предоплата внесена в размере:</td>
-                        <td class="f-b">120 белорусских рублей.</td>
-                    </tr>
-                </tbody>
-                </table>
-            </page>
-        `;
+        $result = "<page style='font-size: 13pt;'>
+        <table>
+        <tbody>
+        <tr>
+        <td colspan='2' class='title'>ПОДТВЕРЖДЕНИЕ БРОНИРОВАНИЯ</td>
+        </tr>
+        </tbody>
+        </table>
+        </page>
+        ";
         return $result;
     }
 }
