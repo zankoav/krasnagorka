597bcc0417e552509b1d5e76f7147228457f3a40
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-09 10:45:58 +0300

    nj

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -435,7 +435,7 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
     }
 
     private static function isAvailableBabyBed($days, $calendarId, $houseId, $isTerem){
-        $result = true;
+        $result = [];
         $bookingSettings = get_option('mastak_booking_appearance_options');
         $babyBedTotalCount = !empty($bookingSettings['baby_bed_count']) ? intval($bookingSettings['baby_bed_count']) : 0;
 
