cd63599d0b6cc19d1966660da13ce05a159bf4c3
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-19 18:29:12 +0300

    dsbsdb

--- L-S/classes/FactoryException.php
+++ L-S/classes/FactoryException.php
@@ -6,13 +6,13 @@ class FactoryException extends Exception {
 
     protected $type;
 
-    public function __construct($message, $type, $code = 0) {
-		parent::__construct($message, $code);
+    public function __construct($message, $type) {
+		parent::__construct($message);
         $this->type = $type;
 	}
 
-    public function getMessage(){
-        return 'gg';//"[$this->type]: $this->message";
-    }
+    // public function getMessage(){
+    //     return 'gg';//"[$this->type]: $this->message";
+    // }
 
 }
