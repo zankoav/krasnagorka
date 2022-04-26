792e15ddfae2dd0b03783f4acb9791de1cf50784
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-26 15:40:05 +0300

    sdbsdb

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -118,7 +118,7 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
     }
 
     public static function calculateResult($request){
-        Log::info('OK 1', $request);
+        Log::info('OK 1', $request['house']);
         $seasonsIntervals = [];
         $houseId = $request['house'];
         $dateStart = $request['dateStart'];
