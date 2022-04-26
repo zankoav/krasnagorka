ad9aee3dd04de8aa02278219ebb84e55ab8e93fc
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-26 19:28:29 +0300

    ить

--- L-S/classes/Order.php
+++ L-S/classes/Order.php
@@ -58,41 +58,4 @@ class Order {
     public int $foodLunch;
     public int $foodDinner;
     public array $sourceValue;
-
-/*
-    //AmoCrm 
-    public string $sbc_order_passport;
-    public int $sbc_order_count_people;
-    public string $sbc_lead_id; // TODO: need to update name
-    public string $sbc_webpay_transaction_id; // TODO: need to update name
-    public string $sbc_task_id; // TODO: need to update name
-
-    //Payment 
-    public string $sbc_order_payment_method;
-    public int $sbc_order_prepaid_percantage;
-    public string $sbc_order_prepaid_source;
-    public string $sbc_order_prepaid_value;
-
-    //Info 
-    public string $sbc_order_client;
-    public string $sbc_order_select;
-    public string $sbc_order_taxonomy_select;
-    public string $sbc_order_taxonomy_check;
-    public string $sbc_order_start;
-    public string $sbc_order_end;
-    public string $sbc_order_price;
-    public string $sbc_order_prepaid;
-    public string $sbc_order_desc;
-    public int $sbc_order_people_count;
-    public int $sbc_order_childs;
-    public int $sbc_order_bath_house_white;
-    public int $sbc_order_bath_house_black;
-    public int $sbc_order_small_animlas_count;
-    public int $sbc_order_big_animlas_count;
-    public bool $sbc_order_baby_bed;
-    public int $sbc_order_food_breakfast;
-    public int $sbc_order_food_lunch;
-    public int $sbc_order_food_dinner;
-*/
-
 }
