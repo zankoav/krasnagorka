854f8a98aa297ab5badff6682eb75b9909ccc2c0
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-26 13:52:14 +0300

    sdvsdvs

--- rest/AMOCRM_Controller.php
+++ rest/AMOCRM_Controller.php
@@ -92,7 +92,6 @@ class AMOCRM_Controller extends WP_REST_Controller {
             $order = OrderFactory::initOrderByRequest($request);
             $response = OrderFactory::getOrderData($order);
             // $data->house = $data->houseId;
-            // $response = LS_Booking_Form_Controller::calculateResult((array)$data);
             
             // if(OrderFactory::isAvailableOrder($order)){
             //     OrderFactory::createOrder($order);
