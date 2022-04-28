c6aee810d43278af98f58b8c7b946218cbb7216d
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-28 19:03:48 +0300

    sdbs

--- L-S/classes/AmoCrmException.php
+++ L-S/classes/AmoCrmException.php
@@ -7,7 +7,7 @@ use LsFactory\FactoryException;
 class AmoCrmException extends FactoryException {
 
     public function __construct($message, $code = 0) {
-		parent::__construct($message, 'ContactException', $code);
+		parent::__construct($message, 'AmoCrmException', $code);
 	}
 
 }
--- L-S/classes/index.php
+++ L-S/classes/index.php
@@ -6,12 +6,12 @@ include_once __DIR__ . '/OrderException.php';
 include_once __DIR__ . '/AmoCrmException.php';
 include_once __DIR__ . '/MailException.php';
 
-include_once __DIR__ . '/Contact.php';
-include_once __DIR__ . '/ContactFactory.php';
+// include_once __DIR__ . '/Contact.php';
+// include_once __DIR__ . '/ContactFactory.php';
 
-include_once __DIR__ . '/Order.php';
-include_once __DIR__ . '/OrderFactory.php';
+// include_once __DIR__ . '/Order.php';
+// include_once __DIR__ . '/OrderFactory.php';
 
-include_once __DIR__ . '/AmoCrmFactory.php';
+// include_once __DIR__ . '/AmoCrmFactory.php';
 
-include_once __DIR__ . '/MailFactory.php';
+// include_once __DIR__ . '/MailFactory.php';
