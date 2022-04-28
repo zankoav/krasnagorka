1210ed6de1cef460f9a9b541c47f1db933465eb1
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-28 19:02:29 +0300

    sdbsdb

--- L-S/classes/index.php
+++ L-S/classes/index.php
@@ -1,13 +1,13 @@
 <?php
 
-// include_once __DIR__ . '/FactoryException.php';
-// include_once __DIR__ . '/ContactException.php';
-// include_once __DIR__ . '/OrderException.php';
-// include_once __DIR__ . '/AmoCrmException.php';
-// include_once __DIR__ . '/MailException.php';
+include_once __DIR__ . '/FactoryException.php';
+include_once __DIR__ . '/ContactException.php';
+include_once __DIR__ . '/OrderException.php';
+include_once __DIR__ . '/AmoCrmException.php';
+include_once __DIR__ . '/MailException.php';
 
-// include_once __DIR__ . '/Contact.php';
-// include_once __DIR__ . '/ContactFactory.php';
+include_once __DIR__ . '/Contact.php';
+include_once __DIR__ . '/ContactFactory.php';
 
 // include_once __DIR__ . '/Order.php';
 // include_once __DIR__ . '/OrderFactory.php';
