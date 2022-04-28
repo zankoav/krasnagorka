e8cd7747d4915f1d8cab73f921a278a80f529fc6
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-28 18:57:13 +0300

    sdb

--- L-S/classes/AmoCrmFactory.php
+++ L-S/classes/AmoCrmFactory.php
@@ -5,60 +5,60 @@ use LsFactory\Contact;
 use LsFactory\Order;
 use LsFactory\AmoCrmException;
 
-use AmoCRM\Models\LeadModel;
-use AmoCRM\Collections\Leads\LeadsCollection;
-use AmoCRM\Client\AmoCRMApiClient;
-use League\OAuth2\Client\Token\AccessTokenInterface;
-use AmoCRM\Exceptions\AmoCRMApiException;
-use AmoCRM\Collections\TagsCollection;
-use AmoCRM\Models\TagModel;
-use AmoCRM\Filters\ContactsFilter;
-use AmoCRM\Filters\LeadsFilter;
-use AmoCRM\Models\ContactModel;
-use AmoCRM\Collections\LinksCollection;
-use AmoCRM\Collections\ContactsCollection;
+// use AmoCRM\Models\LeadModel;
+// use AmoCRM\Collections\Leads\LeadsCollection;
+// use AmoCRM\Client\AmoCRMApiClient;
+// use League\OAuth2\Client\Token\AccessTokenInterface;
+// use AmoCRM\Exceptions\AmoCRMApiException;
+// use AmoCRM\Collections\TagsCollection;
+// use AmoCRM\Models\TagModel;
+// use AmoCRM\Filters\ContactsFilter;
+// use AmoCRM\Filters\LeadsFilter;
+// use AmoCRM\Models\ContactModel;
+// use AmoCRM\Collections\LinksCollection;
+// use AmoCRM\Collections\ContactsCollection;
 
-use AmoCRM\Helpers\EntityTypesInterface;
-use AmoCRM\Collections\TasksCollection;
-use AmoCRM\Models\TaskModel;
+// use AmoCRM\Helpers\EntityTypesInterface;
+// use AmoCRM\Collections\TasksCollection;
+// use AmoCRM\Models\TaskModel;
 
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\SelectCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\SelectCustomFieldValueModel;
-use AmoCRM\Models\CustomFieldsValues\SelectCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\SelectCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\SelectCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\SelectCustomFieldValuesModel;
 
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultitextCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\MultitextCustomFieldValueModel;
-use AmoCRM\Models\CustomFieldsValues\MultitextCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultitextCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\MultitextCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\MultitextCustomFieldValuesModel;
 
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\DateCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\DateCustomFieldValueModel;
-use AmoCRM\Models\CustomFieldsValues\DateCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\DateCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\DateCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\DateCustomFieldValuesModel;
 
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\DateTimeCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\DateTimeCustomFieldValueModel;
-use AmoCRM\Models\CustomFieldsValues\DateTimeCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\DateTimeCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\DateTimeCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\DateTimeCustomFieldValuesModel;
 
-use AmoCRM\Models\CustomFieldsValues\TextCustomFieldValuesModel;
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\TextCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\TextCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\TextCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\TextCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\TextCustomFieldValueModel;
 
-use AmoCRM\Models\CustomFieldsValues\NumericCustomFieldValuesModel;
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\NumericCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\NumericCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\NumericCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\NumericCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\NumericCustomFieldValueModel;
 
-use AmoCRM\Models\CustomFieldsValues\PriceCustomFieldValuesModel;
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\PriceCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\PriceCustomFieldValueModel;
+// use AmoCRM\Models\CustomFieldsValues\PriceCustomFieldValuesModel;
+// use AmoCRM\Models\CustomFieldsValues\ValueCollections\PriceCustomFieldValueCollection;
+// use AmoCRM\Models\CustomFieldsValues\ValueModels\PriceCustomFieldValueModel;
 
-use AmoCRM\Collections\CustomFieldsValuesCollection;
+// use AmoCRM\Collections\CustomFieldsValuesCollection;
 
-use AmoCRM\Collections\CatalogElementsCollection;
-use AmoCRM\Filters\CatalogElementsFilter;
-use AmoCRM\Models\CatalogElementModel;
-use AmoCRM\Models\CatalogModel;
+// use AmoCRM\Collections\CatalogElementsCollection;
+// use AmoCRM\Filters\CatalogElementsFilter;
+// use AmoCRM\Models\CatalogElementModel;
+// use AmoCRM\Models\CatalogModel;
 
-use AmoCRM\Collections\NotesCollection;
-use AmoCRM\Models\NoteType\CommonNote;
+// use AmoCRM\Collections\NotesCollection;
+// use AmoCRM\Models\NoteType\CommonNote;
 
 class AmoCrmFactory {
 
