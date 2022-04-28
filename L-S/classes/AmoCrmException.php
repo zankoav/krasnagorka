48692f0c9b68023b513667bf4ea70f66c15d8b30
aleksandr zanko <aleksandr.zanko@enway.com>
2022-04-28 19:03:06 +0300

    sdbsd

--- L-S/classes/AmoCrmException.php
+++ L-S/classes/AmoCrmException.php
@@ -4,7 +4,7 @@ namespace LsFactory;
 
 use LsFactory\FactoryException;
 
-class ContactException extends FactoryException {
+class AmoCrmException extends FactoryException {
 
     public function __construct($message, $code = 0) {
 		parent::__construct($message, 'ContactException', $code);
