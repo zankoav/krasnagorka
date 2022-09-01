2959d274889d5e811709611499d64431ea906bdc
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-01 11:12:09 +0300

    sbsdb

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -647,7 +647,7 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
                         ),
                         array(
                             'key'     => 'sbc_order_end',
-                            'value'   =>  $right[0],
+                            'value'   =>   date("Y-m-d", strtotime($dateEnd)),
                             'type'      =>  'date',
                             'compare' =>  '='   
                         )
