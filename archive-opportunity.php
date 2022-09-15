ea722437a6cefb0126222602d699eddcaf171526
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-15 12:44:13 +0300

    dsvsdv

--- archive-opportunity.php
+++ archive-opportunity.php
@@ -14,6 +14,13 @@ get_template_part("mastak/views/header", "small-view");
     ?>
 </section>
 <section class="b-bgc-wrapper padding-b-1">
+    <div class="b-container content-text">
+        <div class="header-title">
+            <h2 class="header-title__subtitle">ОТДЫХ В БРАСЛАВЕ</h2>
+        </div>
+        <?= wpautop(get_option( 'mastak_opportunities_appearance_options' )['mastak_opportunity_submenu_big_text']);?>
+    </div>
+    <hr>
 	<?php
     if (is_active_sidebar('opportunities-content')) {
         dynamic_sidebar('opportunities-content');
