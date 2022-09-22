c308ee354163e54bb65bb4abd4f21a00cfee8c14
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-22 23:04:34 +0300

    sdv

--- L-S/cmb2/page-orders.php
+++ L-S/cmb2/page-orders.php
@@ -28,7 +28,7 @@ function page_orders_theme_options_metabox() {
 		// 'menu_title'              => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
 		// 'parent_slug'             => 'themes.php', // Make options page a submenu item of the themes menu.
 		'capability'              => 'read', // Cap required to view options-page.
-		// 'position'                => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
+		'position'                => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
 		// 'admin_menu_hook'         => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
 		// 'priority'                => 10, // Define the page-registration admin menu hook priority.
 		// 'display_cb'              => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
