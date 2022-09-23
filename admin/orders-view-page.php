7bada756aef4efee605f8d3ae664171b047e06da
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-23 11:20:22 +0300

    vsdv

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -1,3 +1,29 @@
+<?php 
+    
+    $dateStart = date("Y-m-d");
+    $dateEnd = date("Y-m-d", strtotime('+15 days', $dateStart));
+    // $ordersQuery = new WP_Query;
+    // $orders = $ordersQuery->query(array(
+    //     'post_type' => 'sbc_orders',
+    //     'posts_per_page' => -1,
+    //     'meta_query' => array(
+    //         'relation' => 'AND',
+    //         array(
+    //             'key'     => 'sbc_order_end',
+    //             'value'   => $dateStart,
+    //             'compare' => '>=',
+    //         ),
+    //         array(
+    //             'key'     => 'sbc_order_start',
+    //             'value'   => $dateEnd,
+    //             'compare' => '<='
+    //         )
+    //     )
+    // ));
+
+?>
+
 <div class="wrap">
     <h1 class="wp-heading"><?= get_admin_page_title() ?></h1>
+    <p>Заказы с <?= $dateStart;?> - <?= $dateEnd;?></p>
 </div>
