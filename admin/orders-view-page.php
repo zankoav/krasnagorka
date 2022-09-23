c096179f541df69f02ff1974d0a0922c46875ade
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-23 11:28:45 +0300

    svsv

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -2,28 +2,29 @@
     
     $dateStart = date("Y-m-d");
     $dateEnd = date("Y-m-d", strtotime('+10 days', strtotime($dateStart)));
-    // $ordersQuery = new WP_Query;
-    // $orders = $ordersQuery->query(array(
-    //     'post_type' => 'sbc_orders',
-    //     'posts_per_page' => -1,
-    //     'meta_query' => array(
-    //         'relation' => 'AND',
-    //         array(
-    //             'key'     => 'sbc_order_end',
-    //             'value'   => $dateStart,
-    //             'compare' => '>=',
-    //         ),
-    //         array(
-    //             'key'     => 'sbc_order_start',
-    //             'value'   => $dateEnd,
-    //             'compare' => '<='
-    //         )
-    //     )
-    // ));
+    $ordersQuery = new WP_Query;
+    $orders = $ordersQuery->query(array(
+        'post_type' => 'sbc_orders',
+        'posts_per_page' => -1,
+        'meta_query' => array(
+            'relation' => 'AND',
+            array(
+                'key'     => 'sbc_order_start',
+                'value'   => $dateStart,
+                'compare' => '>=',
+            ),
+            array(
+                'key'     => 'sbc_order_start',
+                'value'   => $dateEnd,
+                'compare' => '<='
+            )
+        )
+    ));
 
 ?>
 
 <div class="wrap">
     <h1 class="wp-heading"><?= get_admin_page_title() ?> </h1>
     <p>Даты: <b><?= date("d.m.Y", strtotime($dateStart))?></b> - <b><?= date("d.m.Y", strtotime($dateEnd))?></b></p>
+    <p>Количество заказов: <b><?= count($orders);?></b></p>
 </div>
