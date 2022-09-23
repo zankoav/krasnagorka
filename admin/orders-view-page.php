a99a0b69978aa4def157a95e153decbdb2439e0e
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-09-23 13:50:15 +0300

    sdvds

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -79,7 +79,7 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-<div class="wrap" style="font-size: 16px;">
+<div class="wrap">
     <h1 class="wp-heading"><?= get_admin_page_title() ?></h1>
     <p>Даты: <b><?= date("d.m.Y", strtotime($dateStart))?></b> - <b><?= date("d.m.Y", strtotime($dateEnd))?></b></p>
     <p>Количество заказов: <b><?= count($orders);?></b></p>
