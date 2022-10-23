4725f59f1430c374e27b18ce3702a74f7e6fa2fd
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-10-23 15:39:37 +0300

    sdvsdv

--- lwc/frontend/components/z/houseInfo/houseInfo.js
+++ lwc/frontend/components/z/houseInfo/houseInfo.js
@@ -5,7 +5,7 @@ export default class HouseInfo extends LightningElement {
     @api settings
 
     get seasons() {
-        return this.settings.seasons.order((s1, s2) => {
+        return this.settings.seasons.sort((s1, s2) => {
             if (s1.order < s2.order) return -1
             else if (s1.order > s2.order) return 1
             else return 0
