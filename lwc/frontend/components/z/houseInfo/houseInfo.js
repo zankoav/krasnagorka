134f54386c32223b7206cf450dc15f36dcf962ec
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-10-23 15:38:27 +0300

    sadfgg

--- lwc/frontend/components/z/houseInfo/houseInfo.js
+++ lwc/frontend/components/z/houseInfo/houseInfo.js
@@ -1,6 +1,14 @@
-import { LightningElement, track, api } from 'lwc';
-import './houseInfo.scss';
+import { LightningElement, track, api } from 'lwc'
+import './houseInfo.scss'
 
 export default class HouseInfo extends LightningElement {
-    @api settings;
+    @api settings
+
+    get seasons() {
+        return this.settings.seasons.order((s1, s2) => {
+            if (s1.order < s2.order) return -1
+            else if (s1.order > s2.order) return 1
+            else return 0
+        })
+    }
 }
