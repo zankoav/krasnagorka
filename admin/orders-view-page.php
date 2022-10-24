ed511f7135797c65f2712490175fbb515be37697
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-10-24 09:39:07 +0300

    sdvsdv

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -167,15 +167,19 @@
             </style>
             <form class="filter-row row g-2 g-lg-3 row-cols-auto needs-validation flex-wrap flex-md-nowrap mb-4" novalidate>
                 <div class="col-12 col-md">
-                    <label for="from" class="form-label">С</label>
-                    <input type="date" placeholder="С" class="form-control form-control-sm" id="from" aria-describedby="validationServerFrom" required>
+                    <div class="d-flex">
+                        <label for="from" class="form-label mr-3">С</label>
+                        <input type="date" placeholder="С" class="form-control form-control-sm" id="from" aria-describedby="validationServerFrom" required>
+                    </div>
                     <div id="validationServerFrom" class="invalid-feedback">
                         Укажите корректную дату
                     </div>
                 </div>
                 <div class="col-12 col-md">
-                    <label for="to" class="form-label">По</label>
-                    <input type="date" placeholder="По" class="form-control form-control-sm" id="to" aria-describedby="validationServerTo" required>
+                    <div class="d-flex">
+                        <label for="to" class="form-label mr-3">По</label>
+                        <input type="date" placeholder="По" class="form-control form-control-sm" id="to" aria-describedby="validationServerTo" required>
+                    </div>
                     <div id="validationServerTo" class="invalid-feedback">
                         Укажите корректную дату
                     </div>
