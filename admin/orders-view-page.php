05c31457f5bdd4f90e6ad1c70fc240519b240b49
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-10-24 11:52:17 +0300

    cxvb

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -283,7 +283,7 @@
 </div>
 
 <script>
-    (()=>{
+    (($)=>{
         let model = <?= json_encode($model)?>
 
         function render(){
@@ -292,6 +292,6 @@
         }
 
         render();
-    })()
+    })(jQuery)
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
