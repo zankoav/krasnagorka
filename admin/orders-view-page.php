6b915b1e21383b4550631230d5d323f13cf4c435
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-11-02 08:39:25 +0300

    sdbsdb

--- admin/orders-view-page.php
+++ admin/orders-view-page.php
@@ -336,6 +336,11 @@
             </tbody>
         </table>
     </div>
+    <style>
+        #orders {
+            display: none;
+        }
+    </style>
     <div id="orders" class="orders"></div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
