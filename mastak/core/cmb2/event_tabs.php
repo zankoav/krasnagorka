df9986a8ba3950f5f91d98858b7b7fad897acd76
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-11-28 20:55:52 +0300

    sdf

--- mastak/core/cmb2/event_tabs.php
+++ mastak/core/cmb2/event_tabs.php
@@ -733,13 +733,9 @@
     }
 
     function people_counts_options( $field ) {
-        Log::info('field', $field->object_id);
-        Log::info('cmb_id', $field->cmb_id);
-        Log::info('group_id', $field->group->id());
-        Log::info('index', $field->group->index);
-        // $items = get_post_meta( $field->object_id, 'mastak_event_tab_type_10_items', true );
-
-        // Log::info('items', $items);
+        $items = get_post_meta( $field->object_id, 'mastak_event_tab_type_10_items', true );
+        $calendar = $items[$field->group->index]['calendar'];
+        Log::info('calendar', $calendar);
         $options = [];
         $options['0'] = 0;
         $options['1'] = 1;
