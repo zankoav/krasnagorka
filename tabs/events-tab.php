6576789458725f8a953a6c44839c25626a4dd1ff
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-07 19:08:10 +0300

    sdbsdb

--- tabs/events-tab.php
+++ tabs/events-tab.php
@@ -8,11 +8,11 @@ function event_tab_order_settings($box_id, $cmb)
     }
 
     $postItems = get_post_meta($post_id, 'mastak_event_tab_type_10_items', 1);
+    $from = get_post_meta($post_id, 'mastak_event_tab_type_10_from', 1);
+    $to = get_post_meta($post_id, 'mastak_event_tab_type_10_to', 1);
     $ids = [];
     $index = 0;
     foreach ($postItems as $postItem) {
-        $from = $postItem['from'];
-        $to = $postItem['to'];
         $calendarId = $postItem['calendar'];
         if ($status = getOrderStatus($calendarId, $from, $to)) {
             $ids[$index] = ['calendar' => $calendarId, 'status' => $status];
