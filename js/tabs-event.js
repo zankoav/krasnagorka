d383d3d85bf7cef2f3f1dc71ffcdc540182c3506
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-09 09:56:48 +0300

    sdb

--- js/tabs-event.js
+++ js/tabs-event.js
@@ -25,121 +25,123 @@ jQuery(document).on('ready', function () {
         initCalculations()
 
         function initCalculations() {
-            $('.js-calculate').on('click', function () {
-                const empty_calendar = 'Выберите календарь'
-                const empty_date_from = 'Выберите дату заезда'
-                const empty_date_to = 'Выберите дату выезда'
+            $('#mastak_event_tab_type_10')
+                .$('.js-calculate')
+                .on('click', function () {
+                    const empty_calendar = 'Выберите календарь'
+                    const empty_date_from = 'Выберите дату заезда'
+                    const empty_date_to = 'Выберите дату выезда'
 
-                const $parent = $(this).parents('.inside.cmb-field-list')
-                const $message = $(this).parent().parent().find('.cmb2-metabox-description')
-                const $spinner = $(this).parent().find('.spinner')
+                    const $parent = $(this).parents('.inside.cmb-field-list')
+                    const $message = $(this).parent().parent().find('.cmb2-metabox-description')
+                    const $spinner = $(this).parent().find('.spinner')
 
-                const from = $('#mastak_event_tab_type_10_from').val()
-                const to = $('#mastak_event_tab_type_10_to').val()
+                    const from = $('#mastak_event_tab_type_10_from').val()
+                    const to = $('#mastak_event_tab_type_10_to').val()
 
-                let $currentPrice
-                const result = {
-                    is_admin_event: true,
-                    dateFrom: from,
-                    dateTo: to
-                }
-                $message.css({ color: '' }).empty()
-                $parent.find('input, select').each(function (index) {
-                    const name = $(this).attr('name')
+                    let $currentPrice
+                    const result = {
+                        is_admin_event: true,
+                        dateFrom: from,
+                        dateTo: to
+                    }
+                    $message.css({ color: '' }).empty()
+                    $parent.find('input, select').each(function (index) {
+                        const name = $(this).attr('name')
 
-                    if (name) {
-                        const calendarId = name.indexOf('[calendar]') > -1
-                        const oldPrice = name.indexOf('[old_price]') > -1
+                        if (name) {
+                            const calendarId = name.indexOf('[calendar]') > -1
+                            const oldPrice = name.indexOf('[old_price]') > -1
 
-                        const peopleCount = 1
-                        const food_breakfast = name.indexOf('[food_breakfast]') > -1
-                        const food_lunch = name.indexOf('[food_lunch]') > -1
-                        const food_dinner = name.indexOf('[food_dinner]') > -1
-                        const food_full = name.indexOf('[food_full]') > -1
+                            const peopleCount = 1
+                            const food_breakfast = name.indexOf('[food_breakfast]') > -1
+                            const food_lunch = name.indexOf('[food_lunch]') > -1
+                            const food_dinner = name.indexOf('[food_dinner]') > -1
+                            const food_full = name.indexOf('[food_full]') > -1
 
-                        const bath_house_black = name.indexOf('[bath_house_black]') > -1
-                        const bath_house_white = name.indexOf('[bath_house_white]') > -1
+                            const bath_house_black = name.indexOf('[bath_house_black]') > -1
+                            const bath_house_white = name.indexOf('[bath_house_white]') > -1
 
-                        if (oldPrice) {
-                            $currentPrice = $(this)
+                            if (oldPrice) {
+                                $currentPrice = $(this)
+                            }
+
+                            let key
+                            if (calendarId) {
+                                key = 'calendarId'
+                                result[key] = $(this).val()
+                            }
+
+                            if (peopleCount) {
+                                key = 'peopleCount'
+                                result[key] = 1
+                            }
+                            if (food_breakfast && $(this).is(':checked')) {
+                                key = 'foodBreakfast'
+                                result[key] = 1
+                            }
+                            if (food_lunch && $(this).is(':checked')) {
+                                key = 'foodLunch'
+                                result[key] = 1
+                            }
+                            if (food_dinner && $(this).is(':checked')) {
+                                key = 'foodDinner'
+                                result[key] = 1
+                            }
+                            if (food_full && $(this).is(':checked')) {
+                                key = 'foodFull'
+                                result['foodBreakfast'] = 1
+                                result['foodLunch'] = 1
+                                result['foodDinner'] = 1
+                            }
+                            if (bath_house_black) {
+                                key = 'bathHouseBlack'
+                                result[key] = $(this).val()
+                            }
+                            if (bath_house_white) {
+                                key = 'bathHouseWhite'
+                                result[key] = $(this).val()
+                            }
                         }
+                    })
 
-                        let key
-                        if (calendarId) {
-                            key = 'calendarId'
-                            result[key] = $(this).val()
-                        }
+                    let error
 
-                        if (peopleCount) {
-                            key = 'peopleCount'
-                            result[key] = 1
+                    if (!result.calendarId) {
+                        error = empty_calendar
+                    } else if (!result.dateFrom) {
+                        error = empty_date_from
+                    } else if (!result.dateTo) {
+                        error = empty_date_to
+                    }
+
+                    if (error) {
+                        $message.css({ color: '#b32d2e;' }).html(error)
+                    } else {
+                        if (!$spinner.hasClass('spinner_show')) {
+                            calculate(result)
                         }
-                        if (food_breakfast && $(this).is(':checked')) {
-                            key = 'foodBreakfast'
-                            result[key] = 1
-                        }
-                        if (food_lunch && $(this).is(':checked')) {
-                            key = 'foodLunch'
-                            result[key] = 1
-                        }
-                        if (food_dinner && $(this).is(':checked')) {
-                            key = 'foodDinner'
-                            result[key] = 1
-                        }
-                        if (food_full && $(this).is(':checked')) {
-                            key = 'foodFull'
-                            result['foodBreakfast'] = 1
-                            result['foodLunch'] = 1
-                            result['foodDinner'] = 1
-                        }
-                        if (bath_house_black) {
-                            key = 'bathHouseBlack'
-                            result[key] = $(this).val()
-                        }
-                        if (bath_house_white) {
-                            key = 'bathHouseWhite'
-                            result[key] = $(this).val()
+                    }
+
+                    async function calculate(data) {
+                        $spinner.addClass('spinner_show')
+                        const response = await fetch(
+                            'https://krasnagorka.by/wp-json/krasnagorka/v1/ls/calculate/',
+                            {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json; charset=utf-8'
+                                },
+                                body: JSON.stringify(data)
+                            }
+                        )
+                        const responseData = await response.json()
+                        if (responseData) {
+                            $currentPrice.val(responseData.total_price)
+                            $spinner.removeClass('spinner_show')
                         }
                     }
                 })
-
-                let error
-
-                if (!result.calendarId) {
-                    error = empty_calendar
-                } else if (!result.dateFrom) {
-                    error = empty_date_from
-                } else if (!result.dateTo) {
-                    error = empty_date_to
-                }
-
-                if (error) {
-                    $message.css({ color: '#b32d2e;' }).html(error)
-                } else {
-                    if (!$spinner.hasClass('spinner_show')) {
-                        calculate(result)
-                    }
-                }
-
-                async function calculate(data) {
-                    $spinner.addClass('spinner_show')
-                    const response = await fetch(
-                        'https://krasnagorka.by/wp-json/krasnagorka/v1/ls/calculate/',
-                        {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json; charset=utf-8'
-                            },
-                            body: JSON.stringify(data)
-                        }
-                    )
-                    const responseData = await response.json()
-                    if (responseData) {
-                        $currentPrice.val(responseData.total_price)
-                        $spinner.removeClass('spinner_show')
-                    }
-                }
-            })
         }
 
         initInputHandler()
