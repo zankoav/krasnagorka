7bb1086c002d11c9fa97b42a2c6cd439196e1284
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-16 17:54:40 +0300

    dbxnfcg

--- L-S/classes/Variant.php
+++ L-S/classes/Variant.php
@@ -11,11 +11,11 @@ class Variant {
 
     public string $title;
 
-    // public bool $breakfast = false;
-    // public bool $lunch = false;
-    // public bool $dinner = false;
-    // public bool $whiteBath = false;
-    // public bool $blackBath = false;
-    // public int $sale = 0;
-    // public int $price = 0;
+    public bool $breakfast = false;
+    public bool $lunch = false;
+    public bool $dinner = false;
+    public bool $whiteBath = false;
+    public bool $blackBath = false;
+    public int $sale = 0;
+    public int $price = 0;
 }
