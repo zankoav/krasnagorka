9c800afe1519ae65f4bd142525e3d6c950c14f3a
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-16 17:51:00 +0300

    sdbsd

--- L-S/classes/VariantFactory.php
+++ L-S/classes/VariantFactory.php
@@ -7,47 +7,47 @@ class VariantFactory {
 
     public static function getVaraintById($id, $settings = []){
         
-        // $variant = new Variant();
-        // $variant->id = $id;
-        // $variant->title = get_post_title($id);
-        // $food = get_post_meta($id, 'variant_food', 1);
-        // if($food) {
-        //     $variant->breakfast = $food['breakfast'] ?? false;
-        //     $variant->lunch = $food['lunch'] ?? false;
-        //     $variant->dinner = $food['dinner'] ?? false;
-        // }
-        // $variant->whiteBath = get_post_meta($id, 'variant_white_bath', 1) == 'on';
-        // $variant->blackBath = get_post_meta($id, 'variant_black_bath', 1) == 'on';
-        // $sale = get_post_meta($id, 'variant_sale', 1);
-        // $variant->sale = !empty($sale) ? intval($sale) : 0;
+        $variant = new Variant();
+        $variant->id = $id;
+        $variant->title = get_post_title($id);
+        $food = get_post_meta($id, 'variant_food', 1);
+        if($food) {
+            $variant->breakfast = $food['breakfast'] ?? false;
+            $variant->lunch = $food['lunch'] ?? false;
+            $variant->dinner = $food['dinner'] ?? false;
+        }
+        $variant->whiteBath = get_post_meta($id, 'variant_white_bath', 1) == 'on';
+        $variant->blackBath = get_post_meta($id, 'variant_black_bath', 1) == 'on';
+        $sale = get_post_meta($id, 'variant_sale', 1);
+        $variant->sale = !empty($sale) ? intval($sale) : 0;
         
-        // if($variant->breakfast){
-        //     $variant->price += !empty($settings['food_breakfast_price']) ? intval($settings['food_breakfast_price']) : 0;
-        // }
+        if($variant->breakfast){
+            $variant->price += !empty($settings['food_breakfast_price']) ? intval($settings['food_breakfast_price']) : 0;
+        }
 
-        // if($variant->lunch){
-        //     $variant->price += !empty($settings['food_lunch_price']) ? intval($settings['food_lunch_price']) : 0;
-        // }
+        if($variant->lunch){
+            $variant->price += !empty($settings['food_lunch_price']) ? intval($settings['food_lunch_price']) : 0;
+        }
 
-        // if($variant->dinner){
-        //     $variant->price += !empty($settings['food_dinner_price']) ? intval($settings['food_dinner_price']) : 0;
-        // }
+        if($variant->dinner){
+            $variant->price += !empty($settings['food_dinner_price']) ? intval($settings['food_dinner_price']) : 0;
+        }
 
-        // if($variant->breakfast && $variant->lunch && $variant->dinner){
-        //     $variant->price -= !empty($settings['food_triple_sale_price']) ? intval($settings['food_triple_sale_price']) : 0;
-        // }
+        if($variant->breakfast && $variant->lunch && $variant->dinner){
+            $variant->price -= !empty($settings['food_triple_sale_price']) ? intval($settings['food_triple_sale_price']) : 0;
+        }
 
-        // if($variant->whiteBath){
-        //     $variant->price += !empty($settings['bath_house_white_price']) ? intval($settings['bath_house_white_price']) : 0;
-        // }
+        if($variant->whiteBath){
+            $variant->price += !empty($settings['bath_house_white_price']) ? intval($settings['bath_house_white_price']) : 0;
+        }
 
-        // if($variant->blackBath){
-        //     $variant->price += !empty($settings['bath_house_black_price']) ? intval($settings['bath_house_black_price']) : 0;
-        // }
+        if($variant->blackBath){
+            $variant->price += !empty($settings['bath_house_black_price']) ? intval($settings['bath_house_black_price']) : 0;
+        }
 
-        // $variant->price -= $variant->sale;
+        $variant->price -= $variant->sale;
 
-        return null;// $variant;
+        return $variant;
     }
 
     
