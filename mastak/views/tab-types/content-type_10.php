ce244e2b37756c0ac39a280176fe7f6cfe6e3acf
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-19 21:16:59 +0300

    sdb

--- mastak/views/tab-types/content-type_10.php
+++ mastak/views/tab-types/content-type_10.php
@@ -1,7 +1,9 @@
 <?php
     /**
      * @var Type_10 $tab
+     * @var Assets $assets
      */
+    global $assets;
 ?>
 <div class="accordion-mixed__content-inner" data-event=<?=$tab->id?>>
     <script src="<?= $assets->js('tab_events'); ?>"></script>
