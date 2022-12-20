862ec6c0f75686f03fb9a8b79a5c3fb585729626
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-20 22:16:23 +0300

    bnm,

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -55,7 +55,7 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
     */ 
     public function event_tab($request){
         $tabId = absint($request['tabId']);
-        $tab = new Type_10($tabId);
+        // $tab = new Type_10($tabId);
         $resutl = [
             'interval_id' => get_post_meta($tabId, 'mastak_event_tab_type_10_interval', 1)
             'variants_id' => get_post_meta($tabId, 'mastak_event_tab_type_10_variants', 1)
