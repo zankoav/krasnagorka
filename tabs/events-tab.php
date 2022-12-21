dd9e38de9ec9146f2139d933fd6e337f52c74e18
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-21 17:35:52 +0300

    sdvsd

--- tabs/events-tab.php
+++ tabs/events-tab.php
@@ -8,8 +8,10 @@ function event_tab_order_settings($box_id, $cmb)
     }
 
     $postItems = get_post_meta($post_id, 'mastak_event_tab_type_10_items', 1);
-    $from = get_post_meta($post_id, 'mastak_event_tab_type_10_from', 1);
-    $to = get_post_meta($post_id, 'mastak_event_tab_type_10_to', 1);
+
+    $intervalId = get_post_meta($post_id, 'mastak_event_tab_type_10_interval', 1);
+    $from =  get_post_meta($intervalId, "season_from", 1);
+    $to = get_post_meta($intervalId, "season_to", 1);
     $ids = [];
     $index = 0;
     foreach ($postItems as $postItem) {
