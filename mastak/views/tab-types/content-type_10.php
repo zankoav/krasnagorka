3143b6dafa2ae32daa63a3331ac6b7b6fb046659
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2022-12-28 18:29:28 +0300

    sdbsdb

--- mastak/views/tab-types/content-type_10.php
+++ mastak/views/tab-types/content-type_10.php
@@ -5,7 +5,7 @@
      */
     global $assets;
 ?>
-<div class="accordion-mixed__content-inner" data-event-tab=<?=$tab->id?> data-event=<?=get_the_ID()?>>
+<div class="accordion-mixed__content-inner accordion-mixed__content-inner_10" data-event-tab=<?=$tab->id?> data-event=<?=get_the_ID()?>>
     <script src="<?= $assets->js('tab_events'); ?>"></script>
 </div>
 
