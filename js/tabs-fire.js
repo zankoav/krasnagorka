8f1b8bf96555a12fb1b1a6979fa1188718014511
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-01-05 20:28:26 +0300

    sdbsd

--- js/tabs-fire.js
+++ js/tabs-fire.js
@@ -8,10 +8,10 @@ jQuery(document).on('ready', function () {
             $('#cmb2-metabox-mastak_event_tab_type_8')
                 .find('.postbox')
                 .each(function (index) {
+                    console.log('index', index)
                     const state = orderedIds[index]
                     const id = `#mastak_event_tab_type_8_items_${index}_calendar`
                     const $calendar = $(this).find(id)
-                    console.log('!',$calendar[0]);
                     if (state && $calendar[0] && $calendar[0].value == state.calendar) {
                         $(this).addClass(`bgc-${state.status}`)
                         $(this).find('.cmb-group-title').addClass(`bgc-${state.status}`)
