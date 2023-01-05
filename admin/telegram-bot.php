175185b91c39aa5acc5d0b1e6bffc3ac511213cd
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-01-05 19:14:34 +0300

    sdbsdb

--- admin/telegram-bot.php
+++ admin/telegram-bot.php
@@ -11,41 +11,44 @@
 <script>
     (($) => {
 
-        const tg ={
-            token: '5949739525:AAED7FFZliBqmxkBuFb0RfFhi271dh7YJIs',
-            chat_id: '1001716089662'
-        };
-
-
-
-        const options = {
-            method: 'POST',
-            url: `https://api.telegram.org/bot${tg.token}/sendMessage`,
-            headers: {accept: 'application/json', 'content-type': 'application/json'},
-            data: {
-                chat_id: tg.chat_id,
-                text: 'Hello',
-                parse_mode: 'HTML',
-                disable_web_page_preview: false,
-                disable_notification: false,
-                reply_to_message_id: null
+        const data = {
+            tg: {
+                token: '5949739525:AAED7FFZliBqmxkBuFb0RfFhi271dh7YJIs',
+                chat_id: '1001716089662'
+            },
+            photo: 'https://krasnagorka.by/wp-content/uploads/2020/05/IMG_8678-2-320x240.jpg',
+            house: {
+                link: 'https://krasnagorka.by/dom-na-braslavskih-ozyorah/piligrim/',
+                calendar: 'Пилигрим'
+            },
+            date_from:'06.01.2022',
+            date_to:'08.01.2022',
+            sale: 30,
+            price: {
+                old: 390,
+                new: 310
             }
+            order_link: 'https://krasnagorka.by/booking-form/?eventTabId=10654&booking=9486&calendarId=19&from=2023-01-06&to=2023-01-08&terem=Терем%202',
+            description: 'Комфортный, уютный домик на троих с собственной барбекю зоной и шикарным видом на лес и озеро. Расположен вдали от остальных домов.'
         };
-
+            
 
         $('.button-tg').on('click', function(){
-            sendMessage(template());
-
-            // axios.request(options)
-            //     .then(function (response) {
-            //         console.log(response.data);
-            //     })
-            //     .catch(function (error) {
-            //         console.error(error);
-            //     });
-
+            
+            sendMessage(data);
         });
-        const template = () => {
+        
+         const template = (data) => {
+            return encodeURIComponent(`
+⚡️ <b>Горящее предложение</b> ⚡️ <a href="https://krasnagorka.by/dom-na-braslavskih-ozyorah/piligrim/"><b>Пилигрим</b></a> ⚡️\n
+📌 Комфортный, уютный домик на троих с собственной барбекю зоной и шикарным видом на лес и озеро. Расположен вдали от остальных домов.\n
+📆 Даты: <b>06.01.2022</b> - <b>08.01.2022</b>\n
+❤️ Скидка: <b>30%</b>\n
+💰 Стоимость: <b>310 руб.</b> <s>390 руб.</s>\n
+👉🏻 <a href="https://krasnagorka.by/booking-form/?eventTabId=10654&booking=9486&calendarId=19&from=2023-01-06&to=2023-01-08&terem=Терем%202"><b>ЗАБРОНИРОВАТЬ</b></a>`)
+}
+
+        const template2 = (data) => {
             return encodeURI(`
 ⚡️ <b>Горящее предложение</b> ⚡️ <a href="https://krasnagorka.by/dom-na-braslavskih-ozyorah/piligrim/"><b>Пилигрим</b></a> ⚡️\n
 📌 Комфортный, уютный домик на троих с собственной барбекю зоной и шикарным видом на лес и озеро. Расположен вдали от остальных домов.\n
@@ -53,12 +56,12 @@
 ❤️ Скидка: <b>30%</b>\n
 💰 Стоимость: <b>310 руб.</b> <s>390.00 руб.</s>\n
 👉🏻 <a href="https://krasnagorka.by/booking-form/?`) + encodeURIComponent('eventTabId=10654&booking=9486&calendarId=19&from=2023-01-06&to=2023-01-08&terem=Терем%202"') + encodeURI("><b>ЗАБРОНИРОВАТЬ</b></a>");
-        }        
+}        
         
 
-        function sendMessage(text){ 
-            const photo = 'https://krasnagorka.by/wp-content/uploads/2020/05/IMG_8678-2-320x240.jpg';
-            let url = `https://api.telegram.org/bot${tg.token}/sendPhoto?chat_id=-${tg.chat_id}&photo=${photo}&caption=${text}&parse_mode=HTML`;
+        function sendMessage(data){ 
+            const text = template(data);
+            let url = `https://api.telegram.org/bot${data.tg.token}/sendPhoto?chat_id=-${data.tg.chat_id}&photo=${data.photo}&caption=${text}&parse_mode=HTML`;
             let xht = new XMLHttpRequest();
             xht.open("GET", url);
             xht.send();
