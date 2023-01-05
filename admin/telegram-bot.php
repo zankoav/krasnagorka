6301d05ee32c1896119a9c1e92cc248afb067c1d
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-01-05 15:03:13 +0300

    упц

--- admin/telegram-bot.php
+++ admin/telegram-bot.php
@@ -52,7 +52,7 @@
 Заезд: <b>06.01.2022</b>
 Выезд: <b>08.01.2022</b>
 Скидка: <b>30%</b>
-Стоимость: <b>310 руб.</b> <s>390.00 руб.</s><a href="https://krasnagorka.by/booking-form/?eventTabId=10654&booking=9486&calendarId=19&from=2023-01-06&to=2023-01-08&terem=Терем%202"><b>Забронировать</b></a>
+Стоимость: <b>310 руб.</b> <s>390.00 руб.</s><a href="https://krasnagorka.by/booking-form/"><b>Забронировать</b></a>
 `);
         }
 
