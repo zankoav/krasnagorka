0b47d3f74687f14ca719f1d96c7b5bcb5aeb75bb
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-01-11 17:18:33 +0300

    ывмвм

--- functions.php
+++ functions.php
@@ -528,5 +528,4 @@ function kg_add_remind()
 //END */
 
 require_once __DIR__ . '/menu/orders-menu-item.php';
-require_once __DIR__ . '/menu/telegram-menu-item.php';
 require_once __DIR__ . '/tabs/index.php';
