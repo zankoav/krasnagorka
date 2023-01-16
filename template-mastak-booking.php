70d13fdb37e09f9c8a461a59008e04bea5e227fe
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-01-16 17:30:38 +0300

    sdvsdv

--- template-mastak-booking.php
+++ template-mastak-booking.php
@@ -87,7 +87,7 @@
                                 </div>
                                 <a href="<?= get_the_permalink(); ?>" target="_blank"
                                    class="booking-houses__image-wrapper">
-                                    <img class="booking-houses__image" data-id="<?=$kalendar['picture_id']?>" src="<?= $kalendar['picture']; ?>"
+                                    <img class="booking-houses__image" data-id="<?=$kalendar['picture_id']?>" src="<?= wp_get_attachment_image_url( $kalendar['picture_id'], 'calendar-thumb', false );?>"
                                          alt="<?= $kalendar['title']; ?>">
                                 </a>
                                 <div class="booking-houses__header">
