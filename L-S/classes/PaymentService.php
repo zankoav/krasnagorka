aa30dcb6c28ccc7f601d2111745ed9cc713c4731
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-04-26 17:16:09 +0300

    sdb

--- L-S/classes/PaymentService.php
+++ L-S/classes/PaymentService.php
@@ -16,12 +16,15 @@ class PaymentService {
         $settings = get_option('mastak_theme_options');
         $this->is_enable = $settings['alpha_bank_settings_enabled'] == 'on';
         if($this->is_enable){
-            // $this->is_production = $settings['alpha_bank_settings_production_enabled'] == 'on';
+            Log::info('is_enable',true);
+            $this->is_production = $settings['alpha_bank_settings_production_enabled'] == 'on';
             // $this->username = $this->is_production ? $settings['alpha_bank_settings_username_prod'] : $settings['alpha_bank_settings_username_sandbox'];
             // $this->password = $this->is_production ? $settings['alpha_bank_settings_password_prod'] : $settings['alpha_bank_settings_password_sandbox'];
+        }else{
+            Log::info('is_enable',false);
         }
 
-        Log::info('ok', json_encode($this));
+        
     }
 
 
