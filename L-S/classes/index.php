d9c6a40b6022001c36d8a6f588ce7459744b8a4b
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-04-26 16:55:40 +0300

    sdbsdb

--- L-S/classes/index.php
+++ L-S/classes/index.php
@@ -11,7 +11,7 @@ include_once __DIR__ . '/Contact.php';
 include_once __DIR__ . '/ContactFactory.php';
 
 include_once __DIR__ . '/Order.php';
-include_once __DIR__ . '/PaymentService.php';
+// include_once __DIR__ . '/PaymentService.php';
 include_once __DIR__ . '/OrderFactory.php';
 
 include_once __DIR__ . '/AmoCrmFactory.php';
