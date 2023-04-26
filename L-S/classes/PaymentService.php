3a5ad8af08385cff505c0a789194df44d30bae16
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-04-26 18:17:43 +0300

    sdbsdb

--- L-S/classes/PaymentService.php
+++ L-S/classes/PaymentService.php
@@ -33,7 +33,7 @@ class PaymentService {
 
     public function getLinkForRegisterDo() {
         $amount = 99;
-        $orderNumber = 1009;
+        $orderNumber = 1999;
         return "{$this->base_link}register.do?password={$this->password}&userName={$this->username}&amount={$amount}&language=ru&orderNumber={$orderNumber}&returnUrl={$this->return_url}&pageView={$this->device_type}";
     }
 
