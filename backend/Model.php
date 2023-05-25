3e327476e54391148b55a2ca098b3499ef08aff6
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-05-25 17:55:06 +0300

    sdvd

--- backend/Model.php
+++ backend/Model.php
@@ -40,18 +40,18 @@ class Model extends ModelImpl
     // }
 
     // ok
-    public function getPopupContacts()
-    {
-        $options = $this->themeOptions;
-        return [
-            'a1'      => $options['mastak_theme_options_a1'],
-            'mts'     => $options['mastak_theme_options_mts'],
-            'life'    => $options['mastak_theme_options_life'],
-            'email'   => $options['mastak_theme_options_email'],
-            'time'    => $options['mastak_theme_options_time'],
-            'weekend' => $options['mastak_theme_options_weekend'],
-        ];
-    }
+    // public function getPopupContacts()
+    // {
+    //     $options = $this->themeOptions;
+    //     return [
+    //         'a1'      => $options['mastak_theme_options_a1'],
+    //         'mts'     => $options['mastak_theme_options_mts'],
+    //         'life'    => $options['mastak_theme_options_life'],
+    //         'email'   => $options['mastak_theme_options_email'],
+    //         'time'    => $options['mastak_theme_options_time'],
+    //         'weekend' => $options['mastak_theme_options_weekend'],
+    //     ];
+    // }
 
     private function updateWeather()
     {
@@ -103,89 +103,89 @@ class Model extends ModelImpl
     }
 
      // ok
-    public function getMainMenu()
-    {
-        $menuItemsChildren = [];
-        $menuItemsParents  = [];
-        $items             = wp_get_nav_menu_items(3);
-        foreach ($items as $item) {
-            if ($item->menu_item_parent == 0) {
-                $menuItemsParents[$item->ID] = [
-                    'key'   => $item->ID,
-                    'label' => $item->title,
-                    'href'  => $item->url,
-                ];
-            } else {
-                $menuItemsChildren[] = [
-                    'key'    => $item->ID,
-                    'label'  => $item->title,
-                    'href'   => $item->url,
-                    'parent' => $item->menu_item_parent
-                ];
-            }
-        }
+    // public function getMainMenu()
+    // {
+    //     $menuItemsChildren = [];
+    //     $menuItemsParents  = [];
+    //     $items             = wp_get_nav_menu_items(3);
+    //     foreach ($items as $item) {
+    //         if ($item->menu_item_parent == 0) {
+    //             $menuItemsParents[$item->ID] = [
+    //                 'key'   => $item->ID,
+    //                 'label' => $item->title,
+    //                 'href'  => $item->url,
+    //             ];
+    //         } else {
+    //             $menuItemsChildren[] = [
+    //                 'key'    => $item->ID,
+    //                 'label'  => $item->title,
+    //                 'href'   => $item->url,
+    //                 'parent' => $item->menu_item_parent
+    //             ];
+    //         }
+    //     }
 
-        foreach ($menuItemsChildren as $child) {
-            if (empty($menuItemsParents[$child['parent']]['subItems'])) {
-                $menuItemsParents[$child['parent']]['subItems'] = [$child];
-            } else {
-                $menuItemsParents[$child['parent']]['subItems'][] = $child;
-            }
-        }
-        return array_values($menuItemsParents);
-    }
+    //     foreach ($menuItemsChildren as $child) {
+    //         if (empty($menuItemsParents[$child['parent']]['subItems'])) {
+    //             $menuItemsParents[$child['parent']]['subItems'] = [$child];
+    //         } else {
+    //             $menuItemsParents[$child['parent']]['subItems'][] = $child;
+    //         }
+    //     }
+    //     return array_values($menuItemsParents);
+    // }
 
        // ok
-    public function getFooterBottom()
-    {
-        $footer_logo_id  = $this->themeOptions['footer_logo_id'];
-        $footer_logo_src = wp_get_attachment_image_src($footer_logo_id, 'footer-logo')[0];
-        $unp             = wpautop($this->themeOptions['mastak_theme_options_unp']);
-        $unp             = str_replace("\n", "", $unp);
+    // public function getFooterBottom()
+    // {
+    //     $footer_logo_id  = $this->themeOptions['footer_logo_id'];
+    //     $footer_logo_src = wp_get_attachment_image_src($footer_logo_id, 'footer-logo')[0];
+    //     $unp             = wpautop($this->themeOptions['mastak_theme_options_unp']);
+    //     $unp             = str_replace("\n", "", $unp);
 
-        return [
-            "logo"    => $footer_logo_src,
-            'unp'     => $unp,
-            "socials" => [
-                [
-                    'value' => 'insta',
-                    'url'   => $this->themeOptions['mastak_theme_options_instagram'],
-                ],
-                [
-                    'value' => 'fb',
-                    'url'   => $this->themeOptions['mastak_theme_options_facebook'],
-                ],
-                [
-                    'value' => 'ok',
-                    'url'   => $this->themeOptions['mastak_theme_options_odnoklassniki'],
-                ],
-                [
-                    'value' => 'vk',
-                    'url'   => $this->themeOptions['mastak_theme_options_vkontakte'],
-                ],
-                [
-                    'value' => 'youtube',
-                    'url'   => $this->themeOptions['mastak_theme_options_youtube'],
-                ],
-                [
-                    'value' => 'telegram',
-                    'url'   => $this->themeOptions['mastak_theme_options_telegram'],
-                ]
-            ]
-        ];
-    }
+    //     return [
+    //         "logo"    => $footer_logo_src,
+    //         'unp'     => $unp,
+    //         "socials" => [
+    //             [
+    //                 'value' => 'insta',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_instagram'],
+    //             ],
+    //             [
+    //                 'value' => 'fb',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_facebook'],
+    //             ],
+    //             [
+    //                 'value' => 'ok',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_odnoklassniki'],
+    //             ],
+    //             [
+    //                 'value' => 'vk',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_vkontakte'],
+    //             ],
+    //             [
+    //                 'value' => 'youtube',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_youtube'],
+    //             ],
+    //             [
+    //                 'value' => 'telegram',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_telegram'],
+    //             ]
+    //         ]
+    //     ];
+    // }
 
      // ok
-    public function getCurrencies()
-    {
+    // public function getCurrencies()
+    // {
 
-        return [
-            'byn' => 1,
-            'rur' => get_option('rur_currency'),
-            'usd' => get_option('usd_currency'),
-            'eur' => get_option('eur_currency')
-        ];
-    }
+    //     return [
+    //         'byn' => 1,
+    //         'rur' => get_option('rur_currency'),
+    //         'usd' => get_option('usd_currency'),
+    //         'eur' => get_option('eur_currency')
+    //     ];
+    // }
 
     public function baseBookingModel(){
         $bookingSettings = get_option('mastak_booking_appearance_options');
