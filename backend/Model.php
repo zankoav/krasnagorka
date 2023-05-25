39151cea71b212486e778b1e3a1e0bba3f5ddc3e
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-05-25 17:46:15 +0300

    sdvsdv

--- backend/Model.php
+++ backend/Model.php
@@ -23,30 +23,35 @@ use AmoCRM\Models\CustomFieldsValues\NumericCustomFieldValuesModel;
 use AmoCRM\Models\CustomFieldsValues\ValueCollections\NumericCustomFieldValueCollection;
 use AmoCRM\Models\CustomFieldsValues\ValueModels\NumericCustomFieldValueModel;
 
+use LsModel\ModelImpl as ModelImpl;
 
-class Model
+class Model extends ModelImpl
 {
 
-    private $baseModel;
-    private $DAYS = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+    //  // ok
+    // private $themeOptions;
+    //  // ok
+    // private $DAYS = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
 
-    public function __construct()
-    {
-        $this->baseModel = get_option('mastak_theme_options');
-    }
+     // ok
+    // public function __construct()
+    // {
+    //     $this->themeOptions = get_option('mastak_theme_options');
+    // }
 
-    public function getPopupContacts()
-    {
-        $options = $this->baseModel;
-        return [
-            'a1'      => $options['mastak_theme_options_a1'],
-            'mts'     => $options['mastak_theme_options_mts'],
-            'life'    => $options['mastak_theme_options_life'],
-            'email'   => $options['mastak_theme_options_email'],
-            'time'    => $options['mastak_theme_options_time'],
-            'weekend' => $options['mastak_theme_options_weekend'],
-        ];
-    }
+    // ok
+    // public function getPopupContacts()
+    // {
+    //     $options = $this->themeOptions;
+    //     return [
+    //         'a1'      => $options['mastak_theme_options_a1'],
+    //         'mts'     => $options['mastak_theme_options_mts'],
+    //         'life'    => $options['mastak_theme_options_life'],
+    //         'email'   => $options['mastak_theme_options_email'],
+    //         'time'    => $options['mastak_theme_options_time'],
+    //         'weekend' => $options['mastak_theme_options_weekend'],
+    //     ];
+    // }
 
     private function updateWeather()
     {
@@ -97,87 +102,90 @@ class Model
         return $this->updateWeather();
     }
 
-    public function getMainMenu()
-    {
-        $menuItemsChildren = [];
-        $menuItemsParents  = [];
-        $items             = wp_get_nav_menu_items(3);
-        foreach ($items as $item) {
-            if ($item->menu_item_parent == 0) {
-                $menuItemsParents[$item->ID] = [
-                    'key'   => $item->ID,
-                    'label' => $item->title,
-                    'href'  => $item->url,
-                ];
-            } else {
-                $menuItemsChildren[] = [
-                    'key'    => $item->ID,
-                    'label'  => $item->title,
-                    'href'   => $item->url,
-                    'parent' => $item->menu_item_parent
-                ];
-            }
-        }
+     // ok
+    // public function getMainMenu()
+    // {
+    //     $menuItemsChildren = [];
+    //     $menuItemsParents  = [];
+    //     $items             = wp_get_nav_menu_items(3);
+    //     foreach ($items as $item) {
+    //         if ($item->menu_item_parent == 0) {
+    //             $menuItemsParents[$item->ID] = [
+    //                 'key'   => $item->ID,
+    //                 'label' => $item->title,
+    //                 'href'  => $item->url,
+    //             ];
+    //         } else {
+    //             $menuItemsChildren[] = [
+    //                 'key'    => $item->ID,
+    //                 'label'  => $item->title,
+    //                 'href'   => $item->url,
+    //                 'parent' => $item->menu_item_parent
+    //             ];
+    //         }
+    //     }
 
-        foreach ($menuItemsChildren as $child) {
-            if (empty($menuItemsParents[$child['parent']]['subItems'])) {
-                $menuItemsParents[$child['parent']]['subItems'] = [$child];
-            } else {
-                $menuItemsParents[$child['parent']]['subItems'][] = $child;
-            }
-        }
-        return array_values($menuItemsParents);
-    }
+    //     foreach ($menuItemsChildren as $child) {
+    //         if (empty($menuItemsParents[$child['parent']]['subItems'])) {
+    //             $menuItemsParents[$child['parent']]['subItems'] = [$child];
+    //         } else {
+    //             $menuItemsParents[$child['parent']]['subItems'][] = $child;
+    //         }
+    //     }
+    //     return array_values($menuItemsParents);
+    // }
 
-    public function getFooterBottom()
-    {
-        $footer_logo_id  = $this->baseModel['footer_logo_id'];
-        $footer_logo_src = wp_get_attachment_image_src($footer_logo_id, 'footer-logo')[0];
-        $unp             = wpautop($this->baseModel['mastak_theme_options_unp']);
-        $unp             = str_replace("\n", "", $unp);
+       // ok
+    // public function getFooterBottom()
+    // {
+    //     $footer_logo_id  = $this->themeOptions['footer_logo_id'];
+    //     $footer_logo_src = wp_get_attachment_image_src($footer_logo_id, 'footer-logo')[0];
+    //     $unp             = wpautop($this->themeOptions['mastak_theme_options_unp']);
+    //     $unp             = str_replace("\n", "", $unp);
 
-        return [
-            "logo"    => $footer_logo_src,
-            'unp'     => $unp,
-            "socials" => [
-                [
-                    'value' => 'insta',
-                    'url'   => $this->baseModel['mastak_theme_options_instagram'],
-                ],
-                [
-                    'value' => 'fb',
-                    'url'   => $this->baseModel['mastak_theme_options_facebook'],
-                ],
-                [
-                    'value' => 'ok',
-                    'url'   => $this->baseModel['mastak_theme_options_odnoklassniki'],
-                ],
-                [
-                    'value' => 'vk',
-                    'url'   => $this->baseModel['mastak_theme_options_vkontakte'],
-                ],
-                [
-                    'value' => 'youtube',
-                    'url'   => $this->baseModel['mastak_theme_options_youtube'],
-                ],
-                [
-                    'value' => 'telegram',
-                    'url'   => $this->baseModel['mastak_theme_options_telegram'],
-                ]
-            ]
-        ];
-    }
+    //     return [
+    //         "logo"    => $footer_logo_src,
+    //         'unp'     => $unp,
+    //         "socials" => [
+    //             [
+    //                 'value' => 'insta',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_instagram'],
+    //             ],
+    //             [
+    //                 'value' => 'fb',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_facebook'],
+    //             ],
+    //             [
+    //                 'value' => 'ok',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_odnoklassniki'],
+    //             ],
+    //             [
+    //                 'value' => 'vk',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_vkontakte'],
+    //             ],
+    //             [
+    //                 'value' => 'youtube',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_youtube'],
+    //             ],
+    //             [
+    //                 'value' => 'telegram',
+    //                 'url'   => $this->themeOptions['mastak_theme_options_telegram'],
+    //             ]
+    //         ]
+    //     ];
+    // }
 
-    public function getCurrencies()
-    {
+     // ok
+    // public function getCurrencies()
+    // {
 
-        return [
-            'byn' => 1,
-            'rur' => get_option('rur_currency'),
-            'usd' => get_option('usd_currency'),
-            'eur' => get_option('eur_currency')
-        ];
-    }
+    //     return [
+    //         'byn' => 1,
+    //         'rur' => get_option('rur_currency'),
+    //         'usd' => get_option('usd_currency'),
+    //         'eur' => get_option('eur_currency')
+    //     ];
+    // }
 
     public function baseBookingModel(){
         $bookingSettings = get_option('mastak_booking_appearance_options');
@@ -294,7 +302,7 @@ class Model
             'mainContent'   => [
                 "title"         => $title,
                 "type"          => $type,
-                "contractOffer" => $this->baseModel['contract_offer']
+                "contractOffer" => $this->themeOptions['contract_offer']
             ],
             "footerBottom"  => $this->getFooterBottom(),
             "babyBedPrice" => !empty($bookingSettings['baby_bed_price']) ? intval($bookingSettings['baby_bed_price']) : null,
---
+++ backend/ModelImpl.php
@@ -0,0 +1,109 @@
+<?php
+namespace LsModel;
+
+use Ls\Wp\Log as Log;
+
+class ModelImpl
+{
+    protected $themeOptions;
+    protected $DAYS = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+    public function __construct()
+    {
+        $this->themeOptions = get_option('mastak_theme_options');
+    }
+
+    public function getPopupContacts()
+    {
+        return [
+            'a1'      => $this->themeOptions['mastak_theme_options_a1'],
+            'mts'     => $this->themeOptions['mastak_theme_options_mts'],
+            'life'    => $this->themeOptions['mastak_theme_options_life'],
+            'email'   => $this->themeOptions['mastak_theme_options_email'],
+            'time'    => $this->themeOptions['mastak_theme_options_time'],
+            'weekend' => $this->themeOptions['mastak_theme_options_weekend']
+        ];
+    }
+
+    public function getMainMenu()
+    {
+        $menuItemsChildren = [];
+        $menuItemsParents  = [];
+        $items             = wp_get_nav_menu_items(3);
+        foreach ($items as $item) {
+            if ($item->menu_item_parent == 0) {
+                $menuItemsParents[$item->ID] = [
+                    'key'   => $item->ID,
+                    'label' => $item->title,
+                    'href'  => $item->url,
+                ];
+            } else {
+                $menuItemsChildren[] = [
+                    'key'    => $item->ID,
+                    'label'  => $item->title,
+                    'href'   => $item->url,
+                    'parent' => $item->menu_item_parent
+                ];
+            }
+        }
+
+        foreach ($menuItemsChildren as $child) {
+            if (empty($menuItemsParents[$child['parent']]['subItems'])) {
+                $menuItemsParents[$child['parent']]['subItems'] = [$child];
+            } else {
+                $menuItemsParents[$child['parent']]['subItems'][] = $child;
+            }
+        }
+        return array_values($menuItemsParents);
+    }
+
+    public function getFooterBottom()
+    {
+        $footer_logo_id  = $this->themeOptions['footer_logo_id'];
+        $footer_logo_src = wp_get_attachment_image_src($footer_logo_id, 'footer-logo')[0];
+        $unp             = wpautop($this->themeOptions['mastak_theme_options_unp']);
+        $unp             = str_replace("\n", "", $unp);
+
+        return [
+            "logo"    => $footer_logo_src,
+            'unp'     => $unp,
+            "socials" => [
+                [
+                    'value' => 'insta',
+                    'url'   => $this->themeOptions['mastak_theme_options_instagram'],
+                ],
+                [
+                    'value' => 'fb',
+                    'url'   => $this->themeOptions['mastak_theme_options_facebook'],
+                ],
+                [
+                    'value' => 'ok',
+                    'url'   => $this->themeOptions['mastak_theme_options_odnoklassniki'],
+                ],
+                [
+                    'value' => 'vk',
+                    'url'   => $this->themeOptions['mastak_theme_options_vkontakte'],
+                ],
+                [
+                    'value' => 'youtube',
+                    'url'   => $this->themeOptions['mastak_theme_options_youtube'],
+                ],
+                [
+                    'value' => 'telegram',
+                    'url'   => $this->themeOptions['mastak_theme_options_telegram'],
+                ]
+            ]
+        ];
+    }
+
+    public function getCurrencies()
+    {
+
+        return [
+            'byn' => 1,
+            'rur' => get_option('rur_currency'),
+            'usd' => get_option('usd_currency'),
+            'eur' => get_option('eur_currency')
+        ];
+    }
+}
