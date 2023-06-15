e456181673367097f8b753787a1d3d9b7d3ad738
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-06-15 11:20:45 +0300

    sd

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -890,11 +890,11 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
         ];
 
 
-        if($dateStart == date("Y-m-d") && !$data['enabled']){
+        // if($dateStart == date("Y-m-d") && !$data['enabled']){
             // $data['enabled'] = true;
             // $data['message'] = $isOrderWithDayInDayMessage;
-            $data['hide_upper'] = true;
-        }
+            // $data['hide_upper'] = true;
+        // }
 
         return $data;
     }
