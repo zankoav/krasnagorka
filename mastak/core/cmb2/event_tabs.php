0e2c01fcf10092776de73414fc876373ab784cf5
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-06-22 10:19:17 +0300

    nk

--- mastak/core/cmb2/event_tabs.php
+++ mastak/core/cmb2/event_tabs.php
@@ -529,7 +529,7 @@
 
     function cmb2_render_calculate( $field, $escaped_value, $object_id, $object_type, $field_type_object ) {
 		
-        $view = '<div class="calculate-field"><div><button type="button" class="js-calculate button-secondary">Расчитать</button><span class="spinner"></span></div><input type="hidden" name="GG"><p class="cmb2-metabox-description"></p></div>';
+        $view = '<div class="calculate-field"><div><button type="button" class="js-calculate button-secondary">Расчитать</button><span class="spinner"></span></div><input type="hidden"><p class="cmb2-metabox-description"></p></div>';
         $view .= $field_type_object->_desc( true );
         echo $view;
     }
