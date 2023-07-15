e6c7c4628cf45968189ae7de8907381ee0d9a3f3
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-07-15 09:21:52 +0300

    sdfv

--- LS/backend/rest/LS_Booking_Form_Controller.php
+++ LS/backend/rest/LS_Booking_Form_Controller.php
@@ -325,7 +325,7 @@ class LS_Booking_Form_Controller extends WP_REST_Controller
 
             if(!empty($intervallId)){
                 $dateStart = get_post_meta($intervallId, "season_from", true);
-                $dateStart = date("Y-m-d", strtotime('+1 day', strtotime( $dateStart)));
+                $dateStart = date("Y-m-d", strtotime( $dateStart));
                 $dateEnd = get_post_meta($intervallId, "season_to", true);
                 $dateEnd = date("Y-m-d", strtotime($dateEnd));
             }
