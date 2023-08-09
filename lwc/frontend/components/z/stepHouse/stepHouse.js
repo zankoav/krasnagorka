1362b2df79642f0d4217bbfe0ccfc913dfbd93b9
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-08-09 14:08:20 +0300

    bhjk

--- lwc/frontend/components/z/stepHouse/stepHouse.js
+++ lwc/frontend/components/z/stepHouse/stepHouse.js
@@ -17,9 +17,9 @@ export default class StepHouse extends LightningElement {
 
     get peopleCountLabel(){
         let result = "Количество человек"
-        if(this.settings.eventId){
-            result = "Количество взрослых"
-        }
+        // if(this.settings.eventId){
+        //     result = "Количество взрослых"
+        // }
         return result;
     }
 
