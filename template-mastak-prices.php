985c4b688d1d37ea809910f508408a8243f70a50
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-09-27 11:30:20 +0300

    sdsdb

--- template-mastak-prices.php
+++ template-mastak-prices.php
@@ -13,6 +13,7 @@ if (!defined('ABSPATH')) {
 
 $current_season_id = get_option('mastak_theme_options')['current_season'];
 $seasons           = show_seasons_options();
+$seasons_ids       = getSeasonsForPricePage();
 $houses            = show_house_options();
 $services          = show_service_options();
 asort($services);
@@ -31,20 +32,68 @@ get_template_part("mastak/views/header", "small-view"); ?>
     }
 </style>
 <div class="seasons">
-    <?php foreach ($seasons as $season_id => $season_title) :
-        $hide_season = get_post_meta($season_id, "hide_season_checkbox", true);
-        if ($hide_season == 'on') {
+    <div class="season-item season-item__current">
+        <section class="b-container header-title">
+            <h2 class="header-title__subtitle"><?= $seasons[$current_season_id]; ?></h2>
+            <span class="header-title__current-label">текущий период</span>
+        </section>
+        <div class="b-container js-accordion-content">
+            <div class="prices">
+                <table class="prices__table">
+                    <tbody>
+                        <?php
+
+                        $houses_count    = count($houses);
+                        $index_services  = 1;
+                        foreach ($houses as $house_id => $house_title) :
+                            $house_byn = (float)get_post_meta($current_season_id, "house_price_" . $house_id, true);
+                            $house_price = get_current_price($house_byn);
+
+                        ?>
+                            <tr class="<?= $index_services == $houses_count ? "" : "prices__row"; ?>">
+                                <td class="prices__name prices__name_size_50per">
+                                    <a class="prices__link" href="<?= get_permalink($house_id); ?>" target="_blank">
+                                        <?= $house_title; ?>
+                                    </a>
+                                </td>
+                                <td class="prices__value prices__value_active">
+                                    <span class="house-booking__price-per-men js-currency" data-currency="<?= $currency_name; ?>" data-byn="<?= $house_byn; ?>">
+                                        <?= $house_price; ?>
+                                    </span>
+                                    с человека в сутки
+                                </td>
+                            </tr>
+                        <?php
+                            $index_services++;
+                        endforeach; ?>
+                    </tbody>
+
+                </table>
+            </div>
+
+        </div>
+    </div>
+    <div class="b-container content-text season-text">
+        <div class="textwidget">
+            <?= wpautop(get_option('mastak_price_appearance_options')['mastak_price_submenu_big_text']); ?>
+        </div>
+    </div>
+    <?php foreach ($seasons_ids as $season_id) :
+        if ($season_id == $current_season_id) {
             continue;
         }
-        $order_data = get_post_meta($season_id, "season_order", true);
-        $season_order = -1 * (int)(empty($order_data) ? 0 : $order_data);
+        $season_title = $seasons[$season_id];
+        // => $season_title
+        // $hide_season = get_post_meta($season_id, "hide_season_checkbox", true);
+        // if ($hide_season == 'on') {
+        //     continue;
+        // }
+        // $order_data = get_post_meta($season_id, "season_order", true);
+        // $season_order = -1 * (int)(empty($order_data) ? 0 : $order_data);
     ?>
-        <div <?= $current_season_id != $season_id ? "style=\"order:$season_order;\"" : ''; ?> class="season-item <?= $current_season_id == $season_id ? 'season-item__current' : 'js-accordion' ?>">
+        <div class="season-item js-accordion">
             <section class="b-container header-title">
                 <h2 class="header-title__subtitle"><?= $season_title; ?></h2>
-                <?php if ($current_season_id == $season_id) : ?>
-                    <span class="header-title__current-label">текущий период</span>
-                <?php endif; ?>
             </section>
             <div class="b-container js-accordion-content">
                 <div class="prices">
@@ -79,17 +128,8 @@ get_template_part("mastak/views/header", "small-view"); ?>
 
                     </table>
                 </div>
-
             </div>
         </div>
-
-        <?php if ($current_season_id == $season_id) : ?>
-            <div class="b-container content-text season-text">
-                <div class="textwidget">
-                    <?= wpautop(get_option('mastak_price_appearance_options')['mastak_price_submenu_big_text']); ?>
-                </div>
-            </div>
-        <?php endif; ?>
     <?php endforeach; ?>
     <div class="seasons__added">
         <div class="js-accordion">
