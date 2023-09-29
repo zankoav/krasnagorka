df2f72b9ea56ded7aae7c7bb51c54d3f515b155f
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-09-29 12:41:01 +0300

    hjk

--- lwc/frontend/components/z/weather/weather.js
+++ lwc/frontend/components/z/weather/weather.js
@@ -6,4 +6,20 @@ import './weather.scss';
 export default class Weather extends LightningElement {
 
     @api weather;
+
+    get today(){
+        return this.weather[0];
+    }
+
+    get firstDay(){
+        return this.weather[1];
+    }
+
+    get secondDay(){
+        return this.weather[2];
+    }
+
+    get thirdDay(){
+        return this.weather[3];
+    }
 }
