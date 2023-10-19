8ed010f1d168f7f57c3ca310d53801cdd110dbf2
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-10-19 11:19:48 +0300

    AAA

--- backend/PackageCalculate.php
+++ backend/PackageCalculate.php
@@ -1,15 +1,18 @@
 <?php
+
 namespace LsCalculate;
 
 use Ls\Wp\Log as Log;
 
 class PackageCalculate extends CalculateImpl
 {
-    protected function scenario(){
+    protected function scenario()
+    {
         return 'Package';
     }
 
-    protected function calculate($request){
+    protected function calculate($request)
+    {
 
         $packageId = $request['packageId'];
         $calendarId = $request['calendarId'];
@@ -20,20 +23,21 @@ class PackageCalculate extends CalculateImpl
         $isTeremRoom = get_term_meta($calendarId, 'kg_calendars_terem', 1) == 'on';
         $peopleCount = $request['peopleCount'];
 
-        $min_night = intval(get_post_meta($packageId,'package_night_min', 1));
+        $min_night = intval(get_post_meta($packageId, 'package_night_min', 1));
 
-        $calendars = get_post_meta($packageId,'package_calendars', 1);
+        $calendars = get_post_meta($packageId, 'package_calendars', 1);
         $title = get_the_title($packageId);
-        $calendarsFormatted = [];
         $min_people;
         $price_person_night;
         $price_person_night_weekend;
 
         foreach ((array) $calendars as $key => $entry) {
-            if($calendarId == intval($entry['calendar'])){
+            if ($calendarId == intval($entry['calendar'])) {
                 $min_people = intval($entry['package_people_min']);
-                $price_person_night = floatval($entry['package_price']);
-                $price_person_night_weekend = floatval($entry['package_price_weekend']);
+                $price_person_night = str_replace(",", ".", $entry['package_price']);
+                $price_person_night = floatval($price_person_night);
+                $price_person_night_weekend = str_replace(",", ".", $entry['package_price_weekend']);
+                $price_person_night_weekend = floatval($price_person_night_weekend);
                 break;
             }
         }
@@ -43,25 +47,26 @@ class PackageCalculate extends CalculateImpl
         $period = new \DatePeriod(
             new \DateTime($dateStart),
             new \DateInterval('P1D'),
-            $dateEndDT->modify( '+1 day' )
+            $dateEndDT->modify('+1 day')
         );
 
+        $daysCount = iterator_count($period);
         $daysWorkday = [];
         $daysWeekend = [];
 
-        $weekendsStr = get_post_meta($packageId,'package_weekends', 1);
+        $weekendsStr = get_post_meta($packageId, 'package_weekends', 1);
         $weekends = [];
-        if(!empty($weekendsStr)){
+        if (!empty($weekendsStr)) {
             $weekends = preg_split('/\r\n|\r|\n/', $weekendsStr);
         }
 
         foreach ($period as $key => $value) {
-            $day = $value->format('Y-m-d'); 
-           
-            if(in_array($day, $weekends)){
+            $day = $value->format('Y-m-d');
+
+            if (in_array($day, $weekends)) {
                 $daysWeekend[] = $day;
-            }else{
-                $daysWorkday[] = $day;  
+            } else {
+                $daysWorkday[] = $day;
             }
         }
 
@@ -71,20 +76,20 @@ class PackageCalculate extends CalculateImpl
 
 
 
-        $services = get_post_meta($packageId,'package_services', 1);
+        $services = get_post_meta($packageId, 'package_services', 1);
         $servicesFormatted = [];
 
         foreach ((array) $services as $key => $entry) {
             if (isset($entry['service'])) {
-                if($entry['service'] == '1'){
+                if ($entry['service'] == '1') {
                     $servicesFormatted[] = [
                         'id' => '1',
                         'title' => "Количество приемов пищи на 1 чел.",
-                        'count' => intval(2 + ($totalDays - 1 ) * 3)
+                        'count' => intval(2 + ($totalDays - 1) * 3)
                     ];
                 }
 
-                if($entry['service'] == '2'){
+                if ($entry['service'] == '2') {
                     $servicesFormatted[] = [
                         'id' => '2',
                         'title' => 'Количество сеансов на квадроцикле',
@@ -92,7 +97,7 @@ class PackageCalculate extends CalculateImpl
                     ];
                 }
 
-                if($entry['service'] == '3'){
+                if ($entry['service'] == '3') {
                     $servicesFormatted[] = [
                         'id' => '3',
                         'title' => 'Количество сеансов в кедровой бочке',
@@ -100,7 +105,7 @@ class PackageCalculate extends CalculateImpl
                     ];
                 }
 
-                if($entry['service'] == '4'){
+                if ($entry['service'] == '4') {
                     $servicesFormatted[] = [
                         'id' => '4',
                         'title' => 'Количество сеансов на канатной дороге',
@@ -108,7 +113,7 @@ class PackageCalculate extends CalculateImpl
                     ];
                 }
 
-                if($entry['service'] == '5'){
+                if ($entry['service'] == '5') {
                     $seanseCount = $peopleCount < 4 ? 1 : 2;
                     $servicesFormatted[] = [
                         'id' => '5',
@@ -118,15 +123,15 @@ class PackageCalculate extends CalculateImpl
                 }
             }
         }
-        
-        
+
+
         $error = false;
 
-        if($peopleCount < $min_people){
+        if ($peopleCount < $min_people) {
             $error = 'Хакеры!!! Минимальное количество человек';
         }
 
-        if($daysCount < $min_night){
+        if ($daysCount < $min_night) {
             $error = 'Хакеры!!! Минимальное количество ночей';
         }
 
@@ -139,7 +144,7 @@ class PackageCalculate extends CalculateImpl
                 'enabled' => false
             ],
             'title' => $title,
-            'id'=> $packageId,
+            'id' => $packageId,
             'services' => $servicesFormatted,
             'accommodation' => $accomodationPrice,
             'total_price' => $accomodationPrice
@@ -147,27 +152,26 @@ class PackageCalculate extends CalculateImpl
 
         $babyBedAvailable = \LS_Booking_Form_Controller::isAvailableBabyBed($days, $calendarId, $houseId, $isTeremRoom);
 
-        if($babyBedAvailable){
+        if ($babyBedAvailable) {
             $result['baby_bed_available'] = $babyBedAvailable;
 
-            if($babyBed){
+            if ($babyBed) {
                 $bookingSettings = get_option('mastak_booking_appearance_options');
 
                 $babyBedPrice = intval($bookingSettings['baby_bed_price']);
                 $babyBedTotalPrice = $babyBedPrice * $daysCount;
-                
+
                 $result['baby_bed'] = [
                     'total_price' => $babyBedTotalPrice,
                     'price' => $babyBedPrice,
                     'days' => $daysCount,
                     'discount' => 0
                 ];
-    
+
                 $result['total_price'] += $babyBedTotalPrice;
             }
         }
-        
+
         return $result;
     }
-
 }
