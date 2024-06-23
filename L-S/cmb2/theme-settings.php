ca189db481e989221795743b52727c6f62601c04
Aleksandr.Zanko <aleksandr.zanko@fleetcor.com>
2024-06-23 22:32:54 +0300

    vvv

--- L-S/cmb2/theme-settings.php
+++ L-S/cmb2/theme-settings.php
@@ -35,6 +35,9 @@ add_action('cmb2_admin_init', function () {
         'show_option_none' => true,
         'default'          => 'custom',
         'options_cb'       => 'show_seasons_options',
+        'attributes'     => array(
+            'readonly' => 'readonly'
+        )
     ));
 
 
