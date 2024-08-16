f0a008b09d2fdd0a08a21cc5229e22e8c0611075
Aleksandr.Zanko <aleksandr.zanko@fleetcor.com>
2024-08-16 10:18:45 +0300

    sss

--- discounts-shortcodes.php
+++ discounts-shortcodes.php
@@ -2,7 +2,7 @@
 
 function discounts_shortcodes()
 {
-    return get_option('mastak_price_appearance_options')['mastak_price_submenu_big_text'];
+    return wpautop(get_option('mastak_price_appearance_options')['mastak_price_submenu_big_text']);
 }
 
 add_shortcode('discounts', 'discounts_shortcodes');
