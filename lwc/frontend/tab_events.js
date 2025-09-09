736fd5773ceb198877c83cc1c76738bfe011e1ac
Aleksandr.Zanko <aleksandr.zanko@fleetcor.com>
2025-09-09 15:29:48 +0400

    Update

--- lwc/frontend/tab_events.js
+++ lwc/frontend/tab_events.js
@@ -2,7 +2,7 @@ import './common/common.scss'
 import { createElement } from 'lwc'
 import EventApp from 'tab/eventApp'
 
-const wrapper = document.querySelector('.accordion-mixed__content-inner_10')
+const wrapper = document.currentScript.parentNode
 const appEl = createElement('tab-event-app', { is: EventApp })
 appEl.eventTabId = wrapper.getAttribute('data-event-tab')
 appEl.eventId = wrapper.getAttribute('data-event')
