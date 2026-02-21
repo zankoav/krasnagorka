1338f7f3ae2b5386aa5ac69047d700ab5544c894
zankoav <zankoav@mail.ru>
2026-02-21 13:52:16 +0400

    dsvsdv

--- L-S/classes/VariantFactory.php
+++ L-S/classes/VariantFactory.php
@@ -1,30 +1,32 @@
 <?php
+
 namespace LsFactory;
 
 use LsFactory\Variant;
 use Ls\Wp\Log as Log;
 
 
-class VariantFactory {
+class VariantFactory
+{
 
-    public static function getVaraintById($id){
-        
+    public static function getVaraintById($id)
+    {
+
         $variant = new Variant();
         $variant->id = $id;
         $variant->title = get_the_title($id);
-        $variant->pricePerDay = get_post_meta($id, 'variant_price_per_day', 1) ?? "0";
-        $variant->pricePerDay = str_replace(",",".", $variant->pricePerDay );
-        $variant->pricePerDay = floatval($variant->pricePerDay);
+
+        $pricePerDay = get_post_meta($id, 'variant_price_per_day', 1) ?? "0";
+        $pricePerDay = str_replace(",", ".", $pricePerDay);
+        $variant->pricePerDay = floatval($pricePerDay);
 
         $variant->descriptionPerDay = get_post_meta($id, 'variant_description_per_day', 1);
 
         $variant->priceSingle  = intval(get_post_meta($id, 'variant_price_single', 1)) ?? "0";
-        $variant->priceSingle = str_replace(",",".", $variant->priceSingle );
+        $variant->priceSingle = str_replace(",", ".", $variant->priceSingle);
         $variant->priceSingle = floatval($variant->priceSingle);
 
         $variant->descriptionSingle  = get_post_meta($id, 'variant_description_single', 1);
         return $variant;
     }
-
-    
 }
