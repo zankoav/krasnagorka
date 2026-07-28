ce403c590efe30f3b0f7b3368c6fe34756d85336
zankoav <zankoav@mail.ru>
2026-07-28 16:27:12 +0400

    mmm

--- page-podbor-dat.php
+++ page-podbor-dat.php
@@ -3,14 +3,15 @@
     if (!defined('ABSPATH')) {
         exit;
     }
-
+    global $assets;
     get_header('mastak');
     get_template_part("mastak/views/header", "small-view"); ?>
     <div class="b-bgc-wrapper b-py-3">
-        <div class="b-container big-text">
-            <?php the_content(); ?>
-        </div>
+        <div class="b-container free-date-wrapper"></div>
     </div>
 
-<?php get_template_part("mastak/views/footer", "view");
-    get_footer('mastak');
+<?php get_template_part("mastak/views/footer", "view");?>
+
+<script src="<?= $assets->js('free_date'); ?>"></script>
+
+<?php get_footer('mastak');
