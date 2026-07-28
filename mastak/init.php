e2a2cf3b56d368b84c638b084a65c28b5db0609a
zankoav <zankoav@mail.ru>
2026-07-28 16:46:25 +0400

    sss

--- mastak/init.php
+++ mastak/init.php
@@ -332,7 +332,7 @@
             return get_post_thumbnail_id();
 		}else if ( is_404() ) {
             return get_option( 'mastak_theme_options' )['bgimage_404_id'];
-		} else if ( is_page_template('template-payed-success.php') ) {
+		} else if ( is_page_template('template-payed-success.php') || is_page('podbor-dat')) {
 			return get_option( 'mastak_booking_appearance_options' )['mastak_booking_image_id'];
 		}  else {
             return null;
@@ -393,6 +393,10 @@
 		} else if ( is_page_template('template-payed-success.php') ) {
 			echo '<h1 class="main-slide__slide-content-title">',
             'Ваше бронирование',
+			'</h1>';
+		} else if ( is_page('podbor-dat') ) {
+			echo '<h1 class="main-slide__slide-content-title">',
+            'Подбор дат',
 			'</h1>';
 		} else {
 			echo '<h2 class="main-slide__slide-content-title">',
